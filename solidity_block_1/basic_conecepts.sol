69ef4e4b1602046035776aabed9ff4d596f18a66
DangelGlobant <daniel30081990@gmail.com>
2022-01-15 21:18:20 -0300

    23. Creation of the smart contract

--- solidity_block_1/basic_conecepts.sol
+++ solidity_block_1/basic_conecepts.sol
@@ -1,3 +1,7 @@
 // 21. Smart Contract version
-// pragma solidity ^0.4.0;
-pragma solidity >=0.4.0 < 0.7.0;
+// pragma solidity >=0.4.0 < 0.8.11;
+pragma solidity ^0.8.7;
+
+contract FirstContract {
+    
+}
