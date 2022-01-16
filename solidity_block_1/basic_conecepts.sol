39cb471f8bfcd2eeeaac2f772d0f46d2de088cb0
DangelGlobant <daniel30081990@gmail.com>
2022-01-15 21:36:52 -0300

    24. Constructor

--- solidity_block_1/basic_conecepts.sol
+++ solidity_block_1/basic_conecepts.sol
@@ -1,7 +1,15 @@
 // 21. Smart Contract version
 // pragma solidity >=0.4.0 < 0.8.11;
 pragma solidity ^0.8.7;
+import "./ERC20.sol";
 
 contract FirstContract {
-    
+
+    address owner;
+    ERC20Basic token;
+
+    constructor() public{
+        owner = msg.sender;
+        token = new ERC20Basic(1000);
+    }
 }
