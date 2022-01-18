44dff8a7cb6af48fbce313399f65c90ecff3da70
DangelGlobant <daniel30081990@gmail.com>
2022-01-17 21:37:08 -0300

    Finished Navbar componte and start Welcome Section

---
+++ web3.0/client/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+const Welcome = () => {
+  return (
+    <h1>Welcome</h1>
+  );
+}
+
+export default Welcome;
