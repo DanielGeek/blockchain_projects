efdeac058e2eb2a7b33d38cacb13fac3d1d62a33
DangelGlobant <daniel30081990@gmail.com>
2022-01-23 23:13:41 -0300

    Create react context

--- web3.0/client/src/utils/constants.js
+++ web3.0/client/src/utils/constants.js
@@ -1,5 +1,5 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-// address generate when you run them command "npx hardhat run scripts/deploy.js --network ropsten"
+// this is your contract address generate when you run them command "npx hardhat run scripts/deploy.js --network ropsten"
 export const contractAddress = '0x55BAE345f9Ea2dA035813Cb853AAd9e292ae00C6';
