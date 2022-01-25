0e31c0e5591689c6a34d1702bf714221a405b55d
DangelGlobant <daniel30081990@gmail.com>
2022-01-25 10:07:28 -0300

    get data from the form

--- web3.0/client/src/context/TransactionContext.jsx
+++ web3.0/client/src/context/TransactionContext.jsx
@@ -12,15 +12,17 @@ const getEthereumContract = () => {
   const signer = provider.getSigner();
   const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-  console.log({
-    provider,
-    signer,
-    transactionContract
-  });
+  return transactionContract;
 }
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: ''});
+
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
+  }
+
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -55,7 +57,26 @@ export const TransactionProvider = ({ children }) => {
   }
 
   const sendTransaction = async () => {
+    try {
+      if(!ethereum) return alert("Please install metamask");
 
+      const { addressTo, amount, keyword, message } = formData;
+      const transactionContract = getEthereumContract();
+
+      await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [{
+          from: currentAccount,
+          to: addressTo,
+          gas: '0x5208', // 21000 GWEI
+          value: amount, // 0.00001
+        }]
+      })
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object.");
+    }
   }
 
   useEffect(() => {
@@ -63,7 +84,7 @@ export const TransactionProvider = ({ children }) => {
   }, []);
 
   return (
-    <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction }}>
       {children}
     </TransactionContext.Provider>
   )
