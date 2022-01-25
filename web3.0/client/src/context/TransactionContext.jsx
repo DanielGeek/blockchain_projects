30d686952d99b1b09a2ee6664c65b8fb2954c464
DangelGlobant <daniel30081990@gmail.com>
2022-01-24 23:48:19 -0300

    Function context to connect metamask account

--- web3.0/client/src/context/TransactionContext.jsx
+++ web3.0/client/src/context/TransactionContext.jsx
@@ -20,8 +20,50 @@ const getEthereumContract = () => {
 }
 
 export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if(!ethereum) return alert("Please install metamask");
+
+      const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+      if(accounts.length) {
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log('No accounts found');
+      }
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object.");
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      if(!ethereum) return alert("Please install metamask");
+
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
+
+      setConnectedAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object.");
+    }
+  }
+
+  const sendTransaction = async () => {
+
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <TransactionContext.Provider value={{ value: 'test' }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
       {children}
     </TransactionContext.Provider>
   )
