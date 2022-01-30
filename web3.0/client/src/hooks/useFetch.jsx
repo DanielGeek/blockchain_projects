6628f817d0ed7fb85cd1ea55acef9be22cfb1969
DangelGlobant <daniel30081990@gmail.com>
2022-01-30 16:19:01 -0300

    finished the app and refactoring

--- web3.0/client/src/hooks/useFetch.jsx
+++ web3.0/client/src/hooks/useFetch.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_KEY = import.meta.env.VITE_GIPHY_API;
+const APIKEY = import.meta.env.VITE_GIPHY_API;
 
 const useFetch = ({ keyword }) => {
   const [gifUrl, setGifUrl] = useState("");
@@ -9,18 +9,18 @@ const useFetch = ({ keyword }) => {
     try {
       const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`);
       const { data } = await response.json();
-
-      setGifUrl(data[0]?.images?.downsized_medium?.url)
+      if(data[0]) setGifUrl(data[0]?.images?.downsized_medium.url);
+      else setGifUrl("https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284");
     } catch (error) {
-      setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284')
+      setGifUrl("https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284");
     }
-  }
+  };
 
   useEffect(() => {
-    if(keyword) fetchGifs();
+    if (keyword) fetchGifs();
   }, [keyword]);
 
   return gifUrl;
-}
+};
 
 export default useFetch;
