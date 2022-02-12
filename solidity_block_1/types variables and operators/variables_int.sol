4bde7338bf98fb2f9a4b0926a9f91b7b9a71ed69
DangelGlobant <daniel30081990@gmail.com>
2022-02-12 11:56:15 -0300

    31. Integer Variables

---
+++ solidity_block_1/types variables and operators/variables_int.sol
@@ -0,0 +1,22 @@
+// Solidity version
+pragma solidity ^0.8.7;
+
+contract ints {
+    uint my_first_int;
+    uint my_first_int_initialize = 3;
+    uint price = 5000;
+
+    uint8 int_8_bits;
+    uint64 int_64_bits = 7000;
+    uint16 int_16_bits;
+    uint256 int_256_bits;
+
+    int my_first_int_with_signed;
+    int my_number = -32;
+    int my_number_2 = 65;
+
+    int72 int_with_signed_72_bits;
+    int240 int_with_240_bits = 90000;
+    int256 int_with_256_bits;
+
+}
