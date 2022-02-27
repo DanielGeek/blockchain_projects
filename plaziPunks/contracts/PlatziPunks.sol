30756dfb3394f8f1e4b3497a6ea4d17155edee1a
DangelGlobant <daniel30081990@gmail.com>
2022-02-27 19:27:37 -0300

    Using DNA to calculate the image of the NFT

--- plaziPunks/contracts/PlatziPunks.sol
+++ plaziPunks/contracts/PlatziPunks.sol
@@ -7,47 +7,107 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
 import "./PlatziPunksDNA.sol";
 
-contract PlatziPunks is ERC721, ERC721Enumerable {
-  using Counters for Counters.Counter;
+contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
+    using Counters for Counters.Counter;
 
-  Counters.Counter private _idCounter;
-  uint256 public maxSupply;
+    Counters.Counter private _idCounter;
+    uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
-  // The constructor needs two parameters, the name and the acronym of the NFT.
-  constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLPKS") {
-    maxSupply = _maxSupply;
-  }
+    constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLPKS") {
+        maxSupply = _maxSupply;
+    }
 
-  function mint() public {
-    uint256 current = _idCounter.current();
-    require(current < maxSupply, "No PlatziPunks left :(");
+    function mint() public {
+        uint256 current = _idCounter.current();
+        require(current < maxSupply, "No PlatziPunks left :(");
 
-    _safeMint(msg.sender, current);
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
+        _safeMint(msg.sender, current);
+    }
 
-    _idCounter.increment();
-  }
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
 
-  function tokenURI(uint256 tokenId) public view override returns (string memory) {
-    require(_exists(tokenId), "ERC721 Metadata: URI query for nonexistent token");
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
 
-    string memory jsonURI = Base64.encode(
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
+
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+
+        string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "PlatziPunks #',
                 tokenId,
                 '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
-                "// TODO: Calculate image URL",
+                image,
                 '"}'
             )
         );
 
-    return string(abi.encodePacked("data:application/json;base64,", jsonURI));
-  }
+        return
+            string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
 
-  // Override required
-  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    // Override required
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
