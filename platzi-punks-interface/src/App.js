cfa4604ffb060602d60f71dcbf7996fbcb78359f
DangelGlobant <daniel30081990@gmail.com>
2022-03-06 01:24:03 -0300

    Configuring Web3-React

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -1,26 +1,15 @@
-import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
-import Web3 from 'web3';
+import MainLayout from './layouts/main';
 
 function App() {
-	useEffect(() => {
-		if (window.ethereum) {
-			// window.ethereum
-			// 	.request({
-			// 		method: 'eth_requestAccounts',
-			// 	})
-			// 	.then((accounts) => console.log(accounts));
-
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-		}
-	}, []);
 
 	return (
-		<Routes>
-			<Route path='/' exact element={<Home />} />
-		</Routes>
+    <MainLayout>
+      <Routes>
+			  <Route path='/' exact element={<Home />} />
+		  </Routes>
+    </MainLayout>
 	);
 }
 
