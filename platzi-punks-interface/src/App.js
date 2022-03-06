7a4bf777491ebf8da933173dff8f8335a0cedc92
DangelGlobant <daniel30081990@gmail.com>
2022-03-06 00:37:59 -0300

    Managing the connection to Metamask

---
+++ platzi-punks-interface/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './views/home';
+import Web3 from 'web3';
+
+function App() {
+	useEffect(() => {
+		if (window.ethereum) {
+			// window.ethereum
+			// 	.request({
+			// 		method: 'eth_requestAccounts',
+			// 	})
+			// 	.then((accounts) => console.log(accounts));
+
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+		}
+	}, []);
+
+	return (
+		<Routes>
+			<Route path='/' exact element={<Home />} />
+		</Routes>
+	);
+}
+
+export default App;
