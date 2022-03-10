fda06628969558a20cb5a82074555dc38ee7378b
DangelGlobant <daniel30081990@gmail.com>
2022-03-10 16:55:50 -0300

    Detail of PlatziPunk

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
 import Punks from './views/punks';
+import Punk from './views/punk';
 import MainLayout from './layouts/main';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
 			  <Route path='/' exact element={<Home />} />
 				<Route path='/punks' exact element={<Punks />} />
+				<Route path='/punks/:tokenId' exact element={<Punk />} />
 		  </Routes>
     </MainLayout>
 	);
