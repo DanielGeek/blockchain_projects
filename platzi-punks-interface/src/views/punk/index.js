0c8bcf9c6cd67c874e55690583dad59183bff825
DangelGlobant <daniel30081990@gmail.com>
2022-03-10 18:34:31 -0300

    Transferring a PlatziPunk to another account

--- platzi-punks-interface/src/views/punk/index.js
+++ platzi-punks-interface/src/views/punk/index.js
@@ -1,87 +1,140 @@
 import {
-  Stack,
-  Heading,
-  Text,
-  Table,
-  Thead,
-  Tr,
-  Th,
-  Td,
-  Tbody,
-  Button,
-  Tag,
-} from "@chakra-ui/react";
-import { useWeb3React } from "@web3-react/core";
-import RequestAccess from "../../components/request-access";
-import PunkCard from "../../components/punk-card";
-import { usePlatziPunkData } from "../../hooks/usePlatziPunksData";
-import { useParams } from "react-router-dom";
-import Loading from "../../components/loading";
+	Stack,
+	Heading,
+	Text,
+	Table,
+	Thead,
+	Tr,
+	Th,
+	Td,
+	Tbody,
+	Button,
+	Tag,
+	useToast,
+} from '@chakra-ui/react';
+import { useWeb3React } from '@web3-react/core';
+import RequestAccess from '../../components/request-access';
+import PunkCard from '../../components/punk-card';
+import { usePlatziPunkData } from '../../hooks/usePlatziPunksData';
+import { useParams } from 'react-router-dom';
+import Loading from '../../components/loading';
+import { useState } from 'react';
+import usePlatziPunks from '../../hooks/usePlatziPunks';
 
 const Punk = () => {
-  const { active, account } = useWeb3React();
-  const { tokenId } = useParams();
-  const { loading, punk } = usePlatziPunkData(tokenId);
+	const { active, account, library } = useWeb3React();
+	const { tokenId } = useParams();
+	const { loading, punk, update } = usePlatziPunkData(tokenId);
+  const platziPunks = usePlatziPunks();
+	const toast = useToast();
+	const [transfering, setTransfering] = useState();
 
-  if (!active) return <RequestAccess />;
+	const transfer = () => {
+		setTransfering(true);
 
-  if (loading) return <Loading />;
+    const address = prompt("Ingresa la dirección:");
 
-  return (
-    <Stack
-      spacing={{ base: 8, md: 10 }}
-      py={{ base: 5 }}
-      direction={{ base: "column", md: "row" }}
-    >
-      <Stack>
-        <PunkCard
-          mx={{
-            base: "auto",
-            md: 0,
-          }}
-          name={punk.name}
-          image={punk.image}
-        />
-        <Button disabled={account !== punk.owner} colorScheme="green">
-          {account !== punk.owner ? "You are not the owner" : "Transfer"}
-        </Button>
-      </Stack>
-      <Stack width="100%" spacing={5}>
-        <Heading>{punk.name}</Heading>
-        <Text fontSize="xl">{punk.description}</Text>
-        <Text fontWeight={600}>
-          DNA:
-          <Tag ml={2} colorScheme="green">
-            {punk.dna}
-          </Tag>
-        </Text>
-        <Text fontWeight={600}>
-          Owner:
-          <Tag ml={2} colorScheme="green">
-            {punk.owner}
-          </Tag>
-        </Text>
-        <Table size="sm" variant="simple">
-          <Thead>
-            <Tr>
-              <Th>Atributo</Th>
-              <Th>Valor</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {Object.entries(punk.attributes).map(([key, value]) => (
-              <Tr key={key}>
-                <Td>{key}</Td>
-                <Td>
-                  <Tag>{value}</Tag>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </Stack>
-    </Stack>
-  );
+    const isAddress = library.utils.isAddress(address);
+
+    if(!isAddress) {
+      toast({
+        title: "Address invalid",
+        description: "The address is not a address from Etherium",
+        status: "error",
+      });
+      setTransfering(false);
+    } else {
+      platziPunks.methods
+        .safeTransferFrom(punk.owner, address, punk.tokenId)
+        .send({
+          from: account,
+        })
+        .on('error', () => {
+          setTransfering(false);
+        })
+        .on('transactionHash', (txHash) => {
+          toast({
+            title: "Transaction sended",
+            description: txHash,
+            status: "info",
+          });
+        })
+        .on('receipt', () => {
+          setTransfering(false);
+          toast({
+            title: 'Transaction confirmed',
+            description: `The punk now is to ${address}`,
+            status: 'success'
+          });
+          update();
+        })
+    }
+	};
+
+	if (!active) return <RequestAccess />;
+
+	if (loading) return <Loading />;
+
+	return (
+		<Stack
+			spacing={{ base: 8, md: 10 }}
+			py={{ base: 5 }}
+			direction={{ base: 'column', md: 'row' }}
+		>
+			<Stack>
+				<PunkCard
+					mx={{
+						base: 'auto',
+						md: 0,
+					}}
+					name={punk.name}
+					image={punk.image}
+				/>
+				<Button
+					onClick={transfer}
+					disabled={account !== punk.owner}
+					colorScheme='green'
+          isLoading={transfering}
+				>
+					{account !== punk.owner ? 'You are not the owner' : 'Transfer'}
+				</Button>
+			</Stack>
+			<Stack width='100%' spacing={5}>
+				<Heading>{punk.name}</Heading>
+				<Text fontSize='xl'>{punk.description}</Text>
+				<Text fontWeight={600}>
+					DNA:
+					<Tag ml={2} colorScheme='green'>
+						{punk.dna}
+					</Tag>
+				</Text>
+				<Text fontWeight={600}>
+					Owner:
+					<Tag ml={2} colorScheme='green'>
+						{punk.owner}
+					</Tag>
+				</Text>
+				<Table size='sm' variant='simple'>
+					<Thead>
+						<Tr>
+							<Th>Atributo</Th>
+							<Th>Valor</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{Object.entries(punk.attributes).map(([key, value]) => (
+							<Tr key={key}>
+								<Td>{key}</Td>
+								<Td>
+									<Tag>{value}</Tag>
+								</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</Stack>
+		</Stack>
+	);
 };
 
 export default Punk;
