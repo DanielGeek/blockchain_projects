39d6fd2bbe211da7030dbe430b7617b188618662
DangelGlobant <daniel30081990@gmail.com>
2022-03-23 00:23:15 -0300

    events

---
+++ solidity_block_1/platzi_solidity/Asset.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Asset {
+  string public tokenName = "CriptoPlatzi";
+
+  event ChangeName(
+    address editor,
+    string newName
+  );
+
+  function changeName(string memory newName) public {
+    tokenName = newName;
+    emit ChangeName(msg.sender, newName);
+  }
+}
