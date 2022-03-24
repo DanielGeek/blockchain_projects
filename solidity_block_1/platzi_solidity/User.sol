8b2ac3fd6e601bc2e4a656a4364a6f3d2c1cb85a
DangelGlobant <daniel30081990@gmail.com>
2022-03-24 00:19:41 -0300

    enums

---
+++ solidity_block_1/platzi_solidity/User.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract User {
+    enum State { Active, Inactive }
+
+    State public state = State.Inactive;
+
+    function changeState(State newState) public {
+        state = newState;
+    }
+}
