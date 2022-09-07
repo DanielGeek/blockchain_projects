f6744761846b643e24023fa2bcb591a027caaef7
daniel-barreto <daniel30081990@gmail.com>
2022-09-06 22:50:47 -0400

    Variables and data types

--- platzi_solidity/Estructura.sol
+++ platzi_solidity/Estructura.sol
@@ -4,7 +4,13 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Estructura {
 
-    constructor() {
+    int cantidad;
+    uint cantidadSinSigno;
+    address direccion;
+    bool firmado;
 
+    constructor(bool estaFirmado) {
+        direccion = msg.sender;
+        firmado = estaFirmado;
     }
 }
