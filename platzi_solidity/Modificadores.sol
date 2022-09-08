127f8242f1c0452c801b7111fb3e91958df16d7e
daniel-barreto <daniel30081990@gmail.com>
2022-09-07 20:46:13 -0400

    Inheritance

--- platzi_solidity/Modificadores.sol
+++ platzi_solidity/Modificadores.sol
@@ -5,17 +5,20 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Modificadores {
     
     address private owner;
-
-    constructor() {
+    string private nombreOwner;
+    
+    constructor(string memory nombre) {
         owner = msg.sender;
+        nombreOwner = nombre;
     }
-
+    
     function Suma(uint numero1, uint numero2) public view EsOwner() returns (uint) {
         return numero1 + numero2;
     }
-
-    modifier EsOwner {
-        require(msg.sender == owner, "El usuario no es el creador del contrato");
+    
+    modifier EsOwner() {
+        if (msg.sender != owner) revert();
         _;
     }
+    
 }
