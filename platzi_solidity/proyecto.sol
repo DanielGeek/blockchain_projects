dd01ee86ad91e9fe3cd2e1d28e406dc62d572158
daniel-barreto <daniel30081990@gmail.com>
2022-09-12 18:45:28 -0300

    Installing on a testnet

--- platzi_solidity/proyecto.sol
+++ platzi_solidity/proyecto.sol
@@ -2,14 +2,13 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Proyecto is Ownable {
-
+    
     address public comprador;
     address public vendedor;
-
+    address public arbitro;
     bool public depositoListo;
     bool public compradorOK;
     bool public pagoListo;
@@ -17,39 +16,34 @@ contract Proyecto is Ownable {
     uint public montoPago;
 
     modifier onlyComprador() {
-        require(msg.sender == comprador, "No es el comprador");
+        require(msg.sender==comprador,"No es el comprador");
         _;
     }
 
-    constructor(address _comprador, address _vendedor, uint _monto) {
-        comprador = _comprador;
-        vendedor = _vendedor;
-        montoPago = _monto;
-        depositoListo = false;
-        compradorOK = false;
-        pagoListo = false;
+    constructor(address _comprador, address _vendedor, uint _monto, address _arbitro) {
     }
 
-    //deposita el comprador
-    function depositarPago() payable public onlyComprador {
-        require(msg.value == montoPago, "No es el valor correcto");
+    //deposita el comprador 
+    function depositoPago() payable public onlyComprador {
+        require(msg.value == montoPago,"No es el valor correcto");
         depositoListo = true;
     }
 
     function compradorConfirmaOK() public onlyComprador {
-        compradorOK = true;
+        compradorOK=true;
     }
 
-    //retira el vendedor
-    function retirarPago() public {
-        require(compradorOK, "El comprador no ha dado el OK");
+    //retira el vendedor 
+    function retiraPago() public {
         payable(vendedor).transfer(montoPago);
-        pagoListo = true;
+        pagoListo=true;
     }
 
-    //si el vendedor no entrega el producto, interviene el arbitro
+    //si el vendedor no entrega el producto, intervien el arbitro
     function pagarPorArbitro() public onlyOwner {
         payable(vendedor).transfer(montoPago);
-        pagoListo = true;
+        pagoListo=true;
     }
+
+
 }
