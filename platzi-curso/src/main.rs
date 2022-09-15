2c2e1e1e6c3d4e07b93a0829fe8428e3288f007b
daniel-barreto <daniel30081990@gmail.com>
2022-09-15 19:05:12 -0300

    Rust variables and how to display them on the screen

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let name: &str = "Daniel";
+    let mut age: u8 = 32;
+
+    age = age + 1;
+
+    println!("Hello I'm {} and I'm {} years old!", name, age);
 }
