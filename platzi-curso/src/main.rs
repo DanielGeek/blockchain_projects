c79f999d5a6b4a13e944ae8ff984c26c60705988
daniel-barreto <daniel30081990@gmail.com>
2022-09-15 19:47:03 -0300

    Receiving user data

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    print!("Please, enter your name: ");
+    println!("Please, enter your name: ");
 
     let mut name : String = String::new();
     std::io::stdin().read_line(&mut name).unwrap();
     name = name.trim().to_string();
 
-    print!("Please, enter your age: ");
+    println!("Please, enter your age: ");
     let mut age : String = String::new();
     std::io::stdin().read_line(&mut age).unwrap();
 
     let age_int : u8 = age.trim().parse().unwrap();
 
-    print!("Hello, welcome {} of {} years old", name, age_int);
+    println!("Hello, welcome {} of {} years old", name, age_int);
 }
