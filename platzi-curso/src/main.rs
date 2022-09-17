6e430087890f5dd2a8c722151f3b4d9a84daa749
daniel-barreto <daniel30081990@gmail.com>
2022-09-16 22:28:28 -0300

    Interaction with the environment and user data

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -3,42 +3,91 @@ use std::collections::{HashMap};
 use std::{fs};
 
 const FILENAME: &str = "history.csv";
+const FIRST_TAG: &str = "INICIO";
 
 #[derive(Debug)]
-struct DataHistory {
-    data_type: String,
+struct DatoHistoria {
+    tipo_dato: String,
     tag: String,
-    text: String,
-    live: i32
+    texto: String,
+    vida: i32,
+    opciones: Vec<DatoHistoria>,
 }
 
-impl DataHistory {
-    fn new(row: StringRecord) -> DataHistory {
-        let live = row.get(3).unwrap().trim();
-        let live : i32 = live.parse().unwrap_or(0);
-
-        return DataHistory {
-            data_type: row.get(0).unwrap().trim().to_string(),
+impl DatoHistoria {
+    fn new(row: StringRecord) -> DatoHistoria{
+        let vida = row.get(3).unwrap().trim();
+        let vida : i32 = vida.parse().unwrap_or(0);
+        return DatoHistoria {
+            tipo_dato:row.get(0).unwrap().trim().to_string(),
             tag: row.get(1).unwrap().trim().to_string(),
-            text: row.get(2).unwrap().trim().to_string(),
-            live: live,
+            texto: row.get(2).unwrap().trim().to_string(),
+            vida: vida,
+            opciones: vec![],
         };
     }
 }
 
+
+
 fn main() {
-    let mut data_history: HashMap<String, DataHistory> = HashMap::new();
+    let mut vida = 100;
+    let mut tag_actual = FIRST_TAG;
+
+    let mut last_record: String = "".to_string();
+
+    let mut datos_historia: HashMap<String, DatoHistoria> = HashMap::new();
 
     let content = fs::read_to_string(FILENAME).unwrap();
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
     for result in rdr.records() {
         let result = result.unwrap();
-
-        let data = DataHistory::new(result);
-
-        data_history.insert(data.tag.clone(), data);
+        let dato = DatoHistoria::new(result);
+        if dato.tipo_dato == "SITUACION" {
+            let record_tag = dato.tag.clone();
+            datos_historia.insert(record_tag.clone(), dato);
+            last_record = record_tag;
+        }else if dato.tipo_dato == "OPCION" {
+            if let Some(data) = datos_historia.get_mut(&last_record) {
+                (*data).opciones.push(dato);
+            }
+        }
     }
 
-    println!("{:?}", data_history["DERECHA"]);
+    // Game Loop
+    loop {
+        println!("Tienes {} de vida", vida);
+        
+        if let Some(data) = datos_historia.get(tag_actual){
+            println!("{}", data.texto);
+
+            for (indice, option) in data.opciones.iter().enumerate() {
+                println!("[{}] {}", indice, option.texto);
+            }
+
+            let mut seleccion = String::new();
+            std::io::stdin().read_line(&mut seleccion).unwrap();
+            let seleccion = seleccion.trim().parse().unwrap_or(99);
+
+            if let Some(opcion_elegida) = &data.opciones.get(seleccion){
+                tag_actual = &opcion_elegida.tag;
+            }else{
+                println!("Comando no valido");
+            }
+
+
+            vida += data.vida;
+            println!("");
+        }else{
+            break;
+        }
+
+        // Si la vida <= 0 entonces terminar juego
+        if vida <= 0{
+            println!("Has perdido!");
+            break;
+        }
+
+    }
 }
