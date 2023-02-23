fbeca4dc22b706459c2b849b8aa6debed6bb5a8b
Daniel <daniel30081990@gmail.com>
2023-02-22 23:57:39 -0300

    creating our new smart contract marketplace and install openzeppelin

---
+++ nftsmartContract/contracts/NFTMarketplace.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+// INTERNAL IMPORT FOR NFT OPENZEPPELIN
+import "@openzeppelin/contracts/utils/Counters.sol"; // Using as a counter
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+}
