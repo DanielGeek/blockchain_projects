a90c30aa8ff68da092fa1f1edc7ff98ef6891e51
Daniel <daniel30081990@gmail.com>
2023-03-01 14:26:25 -0300

    Finished HeroSection component

--- 11-NFTMarketplace/client/pages/index.js
+++ 11-NFTMarketplace/client/pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Style from '../styles/index.module.css';
+import { HeroSection } from '../components/componentsindex';
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className={Style.homePage}>
+			<HeroSection />
+		</div>
+	);
 };
 
 export default Home;
