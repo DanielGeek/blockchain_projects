9f4995cde0dbdc966ee6d37d17407b79b4aafc7e
Daniel <daniel30081990@gmail.com>
2023-03-05 00:04:12 -0300

    collectionPage components

---
+++ 11-NFTMarketplace/client/collectionPage/NFTCardTwo/NFTCardTwo.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { BsImage } from 'react-icons/bs';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { MdVerified, MdTimer } from 'react-icons/md';
+import Link from 'next/link';
+
+//INTERNAL IMPORT
+import Style from './NFTCardTwo.module.css';
+import { LikeProfile } from '../../components/componentsindex';
+
+const NFTCardTwo = ({ NFTData }) => {
+	return <div>NFTCardTwo</div>;
+};
+
+export default NFTCardTwo;
