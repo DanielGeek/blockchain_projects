da02d541f1f03d82b7f908c99a6007233b458b88
Daniel <daniel30081990@gmail.com>
2023-03-09 22:03:42 -0300

    contact us component

--- 11-NFTMarketplace/client/components/NavBar/HelpCenter/HelpCenter.jsx
+++ 11-NFTMarketplace/client/components/NavBar/HelpCenter/HelpCenter.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 
+//INTERNAL IMPORT
 import Style from './HelpCenter.module.css';
 
 const HelpCenter = () => {
 	const helpCenter = [
 		{
 			name: 'About',
-			link: 'about',
+			link: 'aboutus',
 		},
 		{
 			name: 'Contact Us',
-			link: 'contact-us',
+			link: 'contactus',
 		},
 		{
 			name: 'Sign Up',
-			link: 'sing-up',
+			link: 'signUp',
 		},
 		{
-			name: 'Sign In',
-			link: 'sign-in',
+			name: 'LogIn',
+			link: 'login',
 		},
 		{
 			name: 'Subscription',
@@ -29,7 +30,7 @@ const HelpCenter = () => {
 	return (
 		<div className={Style.box}>
 			{helpCenter.map((el, i) => (
-				<div key={i + 1} className={Style.helpCenter}>
+				<div className={Style.helpCenter} key={i + 1}>
 					<Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
 				</div>
 			))}
