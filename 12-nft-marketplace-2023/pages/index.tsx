b96cbee49caa1f970353632a44eb2e1ee69c7cd0
Daniel <daniel30081990@gmail.com>
2023-06-17 13:52:01 -0400

    13. Nft props

--- 12-nft-marketplace-2023/pages/index.tsx
+++ 12-nft-marketplace-2023/pages/index.tsx
@@ -3,6 +3,7 @@
 
 import type { NextPage } from 'next'
 import { BaseLayout, NftList } from '../components'
+import nfts from "../content/meta.json";
 
 const Home: NextPage = () => {
   return (
@@ -18,7 +19,9 @@ const Home: NextPage = () => {
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
-          <NftList />
+          <NftList
+            nfts={nfts}
+          />
         </div>
       </div>
     </BaseLayout>
