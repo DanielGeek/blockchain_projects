0b4d6ae83625999a06477455d248909f2da5f3e2
Daniel <daniel30081990@gmail.com>
2023-07-22 22:45:31 -0400

    27. Provider

--- 12-nft-marketplace-2023/components/providers/web3/index.tsx
+++ 12-nft-marketplace-2023/components/providers/web3/index.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, createContext, useContext, useEffect, useState } from "react";
 import { Web3State, createDefaultState } from "./utils";
+import { ethers } from "ethers";
 
 const Web3Context = createContext<Web3State>(createDefaultState());
 
@@ -8,15 +9,16 @@ const Web3Provider: FunctionComponent = ({ children }) => {
 
     useEffect(() => {
         function initWeb3() {
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum as any);
                         
             setWeb3Api({
                 ethereum: window.ethereum,
-                provider: null,
+                provider,
                 contract: null,
                 isLoading: false
             })
         }
-
 
         initWeb3();
     }, [])
