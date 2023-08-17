8af35731d23e7f273b23863fe9d0377cc841d07e
Daniel <daniel30081990@gmail.com>
2023-08-17 00:47:46 -0400

    62. Prepare test

--- 12-nft-marketplace-2023/contracts/NftMarket.sol
+++ 12-nft-marketplace-2023/contracts/NftMarket.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
     using Counters for Counters.Counter;
---
+++ 12-nft-marketplace-2023/test/nftMarket.test.js
@@ -0,0 +1,19 @@
+
+const NftMarket = artifacts.require("NftMarket");
+
+contract("NftMarket", accounts => {
+    let _contract = null;
+
+    before(async () => {
+        _contract = await NftMarket.deployed();
+        console.log(accounts);
+    })
+
+    describe('Mint token', () => {
+
+        it("Should resolve into true value", () => {
+            let numberOfNfts = 12;
+            assert(numberOfNfts == 12, "Value is NOT true");
+        });
+    });
+});
