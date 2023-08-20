013f78f9058809ecfa3d15ba593fa3d65b769534
Daniel <daniel30081990@gmail.com>
2023-08-19 22:12:22 -0400

    96. Get Complete Nfts

--- 12-nft-marketplace-2023/components/hooks/web3/useListedNfts.ts
+++ 12-nft-marketplace-2023/components/hooks/web3/useListedNfts.ts
@@ -1,10 +1,11 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import { Nft } from "@_types/nft";
+import { ethers } from "ethers";
 import useSWR from "swr";
 
 type UseListedNftsResponse = {}
 
-type ListedNftsHookFactory = CryptoHookFactory<any, UseListedNftsResponse>
+type ListedNftsHookFactory = CryptoHookFactory<Nft[], UseListedNftsResponse>
 
 export type UseListedNftsHook = ReturnType<ListedNftsHookFactory>
 
@@ -13,9 +14,26 @@ export const hookFactory: ListedNftsHookFactory = ({contract}) => () => {
     const { data, ...swr } = useSWR(
         contract ? "web3/useListedNfts" : null,
         async () => {
+            const nfts = [] as Nft[];
             const coreNfts = await contract!.getAllNftsOnSale();
 
-            const nfts = [] as any;
+            for (let i = 0; i < coreNfts.length; i++) {
+                const item = coreNfts[i];
+                const tokenURI = await contract!.tokenURI(item.tokenId);
+                const PINATA_GATEWAY_TOKEN = process.env.NEXT_PUBLIC_PINATA_GATEWAY_TOKEN as string;
+                const metaRes = await fetch(`${tokenURI}?pinataGatewayToken=${PINATA_GATEWAY_TOKEN}`);
+                const meta = await metaRes.json();
+
+                nfts.push({
+                    price: parseFloat(ethers.utils.formatEther(item.price)),
+                    tokenId: item.tokenId.toNumber(),
+                    creator: item.creator,
+                    isListed: item.isListed,
+                    meta
+                });
+                
+            }
+            
             return nfts;
         }
     )
