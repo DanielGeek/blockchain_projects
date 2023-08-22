a29273e1e2defaaa605463e75599f870b835d55c
Daniel <daniel30081990@gmail.com>
2023-08-22 17:47:56 -0400

    128. Get Price

--- 12-nft-marketplace-2023/pages/nft/create.tsx
+++ 12-nft-marketplace-2023/pages/nft/create.tsx
@@ -14,6 +14,7 @@ const ALLOWED_FIELDS = ["name", "description", "image", "attributes"];
 const NftCreate: NextPage = () => {
     const { ethereum } = useWeb3();
     const [nftURI, setNftURI] = useState("");
+    const [price, setPrice] = useState("");
     const [hasURI, setHasURI] = useState(false);
     const [nftMeta, setNftMeta] = useState<NftMeta>({
         name:"",
@@ -115,7 +116,7 @@ const NftCreate: NextPage = () => {
                 }
             });
 
-            alert("Can create NFT");
+            alert(price);
         } catch (e: any) {
             console.error(e.message);
         }
@@ -195,6 +196,8 @@ const NftCreate: NextPage = () => {
                                             </label>
                                             <div className="mt-1 flex rounded-md shadow-sm">
                                                 <input
+                                                    onChange={(e) => setPrice(e.target.value)}
+                                                    value={price}
                                                     type="number"
                                                     name="price"
                                                     id="price"
