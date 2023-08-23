bc84574da3c9541ca31155ecccc34cb1d86863ce
Daniel <daniel30081990@gmail.com>
2023-08-23 00:09:26 -0400

    138. Migration to Sepolia

--- 12-nft-marketplace-2023/truffle-config.js
+++ 12-nft-marketplace-2023/truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
@@ -6,6 +8,16 @@ module.exports = {
       port: 7545,
       network_id: "*",
     },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        keys.DEPLOYER_KEY,
+        keys.INFURA_SEPOLIA_URL
+      ),
+      network_id: "11155111",
+      gasPrice: 2500000000,
+      networkCheckoutTimeout: 10000,
+      timeoutBlocks: 200
+    }
   },
   compilers: {
     solc: {
