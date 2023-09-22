7eaf0bd51c4838f38bcec6841042ca8d9f397f55
Daniel <daniel30081990@gmail.com>
2023-09-22 00:39:10 -0400

    148. Fixtures

--- 14-adopt-pet-hardhat/test/PetAdaption.js
+++ 14-adopt-pet-hardhat/test/PetAdaption.js
@@ -1,28 +1,27 @@
 
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
 describe("PetAdoption", function() {
 
-    async function deployContract() {
+    async function deployContractFixture() {
         const [owner] = await ethers.getSigners();
         const PetAdoption = await ethers.getContractFactory("PetAdoption");
         const contract = await PetAdoption.deploy();
 
-        return { owner, contract };
+        return { owner, contract};
     }
 
     describe("Deployment", function() {
         it("Should set the right owner", async function() {
-            const { owner, contract } = await deployContract();
+            const { owner, contract } = await loadFixture(deployContractFixture);
             const contractOwner = await contract.owner();
-
             expect(contractOwner).to.equal(owner.address);
         });
 
         it("Should return the right owner", async function() {
-            const { owner, contract } = await deployContract();
+            const { owner, contract } = await loadFixture(deployContractFixture);
             const contractOwner = await contract.getOwner();
-
             expect(contractOwner).to.equal(owner.address);
         });
     });
