9eb7124349a04e625ac7d323b7ce28496f5de248
Daniel <daniel30081990@gmail.com>
2023-10-06 00:25:16 -0400

    155. Console logs

--- 14-adopt-pet-hardhat/test/PetAdaption.js
+++ 14-adopt-pet-hardhat/test/PetAdaption.js
@@ -60,11 +60,15 @@ describe("PetAdoption", function() {
 
             await expect(contract.adoptPet(adoptPetIdx)).to.be.revertedWith("Pet is already adopted");
         });
+
         it("Should adopt pet successfully", async function() {
             const { contract, account2 } = await loadFixture(deployContractFixture);
             const idx = 1;
-
             await expect(contract.connect(account2).adoptPet(idx)).not.to.be.reverted;
+
+            const petOwnerAddress = await contract.petIdxToOwnerAddress(idx);
+            expect(petOwnerAddress).to.equal(account2.address);
+
         });
     });
 });
