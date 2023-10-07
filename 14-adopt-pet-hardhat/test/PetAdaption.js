ea687c0a63af203f7b92a9d10c8740bbdbc25733
Daniel <daniel30081990@gmail.com>
2023-10-07 14:03:45 -0400

    156. Test new getters

--- 14-adopt-pet-hardhat/test/PetAdaption.js
+++ 14-adopt-pet-hardhat/test/PetAdaption.js
@@ -63,11 +63,23 @@ describe("PetAdoption", function() {
 
         it("Should adopt pet successfully", async function() {
             const { contract, account2 } = await loadFixture(deployContractFixture);
-            const idx = 1;
-            await expect(contract.connect(account2).adoptPet(idx)).not.to.be.reverted;
+            const firstPetIdx = 1;
+            const secondPetIdx = 4;
 
-            const petOwnerAddress = await contract.petIdxToOwnerAddress(idx);
+            await expect(contract.connect(account2).adoptPet(firstPetIdx)).not.to.be.reverted;
+            await contract.connect(account2).adoptPet(secondPetIdx);
+
+            const petOwnerAddress = await contract.petIdxToOwnerAddress(firstPetIdx);
             expect(petOwnerAddress).to.equal(account2.address);
+
+            const petsByOwner = await contract.connect(account2).getAllAdoptedPetsByOwner();
+            const allAdoptedPets = await contract.getAllAdoptedPets();
+
+            expect(petsByOwner.length).to.equal(2);
+            expect(allAdoptedPets.length).to.equal(3);
+
+            expect(await contract.ownerAddressToPetList(account2.address, 0)).to.equal(firstPetIdx);
+            expect(await contract.allAdoptedPets(2)).to.equal(secondPetIdx);
 
         });
     });
