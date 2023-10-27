cec11e6e2fcc74fc139a38daa0f2441d63f43685
Daniel <daniel30081990@gmail.com>
2023-10-26 21:46:29 -0400

    172. Save artifacts

--- 14-adopt-pet-hardhat/scripts/deploy.js
+++ 14-adopt-pet-hardhat/scripts/deploy.js
@@ -16,6 +16,28 @@ async function main() {
     await contract.waitForDeployment();
 
     console.log(`PetAdoption deployed to ${contract.target}`);
+
+    saveContractFiles(contract);
+}
+
+function saveContractFiles(contract) {
+    const contractDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+    if (!fs.existsSync(contractDir)) {
+        fs.mkdirSync(contractDir);
+    }
+
+    fs.writeFileSync(
+        path.join(contractDir, `contract-address-${network.name}.json`),
+        JSON.stringify({PetAdoption: contract.target}, null, 2)
+    );
+
+    const PetAdoptionArtifac = artifacts.readArtifactSync("PetAdoption");
+
+    fs.writeFileSync(
+        path.join(contractDir, "PetAdoption.json"),
+        JSON.stringify(PetAdoptionArtifac, null, 2)
+    );
 }
 
 main().catch(error => {
