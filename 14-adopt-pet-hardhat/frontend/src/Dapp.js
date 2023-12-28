5a9b0840f9287ed75da8a10f7598f4ceb227b1ca
Daniel <daniel.passage606@passmail.com>
2023-12-28 00:20:12 -0400

    187. Testing on Mumbai

--- 14-adopt-pet-hardhat/frontend/src/Dapp.js
+++ 14-adopt-pet-hardhat/frontend/src/Dapp.js
@@ -6,7 +6,7 @@ import { WalletNotDetected } from './components/WalletNotDetected';
 import { ConnectWallet } from './components/ConnectWallet';
 
 import { ethers } from 'ethers';
-import contractAddress from './contracts/contract-address-localhost.json';
+import { contractAddress } from "./address";
 import PetAdoptionArtifact from './contracts/PetAdoption.json';
 import { TxInfo } from './components/TxInfo';
 
---
+++ 14-adopt-pet-hardhat/frontend/src/address.js
@@ -0,0 +1,3 @@
+export const contractAddress = process.env.NODE_ENV === "development" ?
+    require("./contracts/contract-address-localhost.json") :
+    require("./contracts/contract-address-polygon_mumbai.json");
