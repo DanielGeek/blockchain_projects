22bafafae793051dcd0c7773f3c497167644233d
Daniel <daniel.passage606@passmail.com>
2024-02-02 22:44:04 -0400

    Install Rainbowkit, Wagmi & Viem

---
+++ 15-aa-chatter/chatter-app/app/providers.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import * as React from 'react';
+import {
+    RainbowKitProvider,
+    getDefaultWallets,
+    connectorsForWallets,
+} from '@rainbow-me/rainbowkit';
+import {
+    argentWallet,
+    trustWallet,
+    ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import {
+    sepolia,
+} from 'wagmi/chains';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [
+        sepolia
+    ],
+    [publicProvider()]
+);
+
+const projectId = 'YOUR_PROJECT_ID';
+
+const { wallets } = getDefaultWallets({
+    appName: 'RainbowKit demo',
+    projectId,
+    chains,
+});
+
+const demoAppInfo = {
+    appName: 'Rainbowkit Demo',
+};
+
+const connectors = connectorsForWallets([
+    ...wallets,
+    {
+        groupName: 'Other',
+        wallets: [
+            argentWallet({ projectId, chains }),
+            trustWallet({ projectId, chains }),
+            ledgerWallet({ projectId, chains }),
+        ],
+    },
+]);
+
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+    webSocketPublicClient,
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const [mounted, setMounted] = React.useState(false);
+    React.useEffect(() => setMounted(true), []);
+    return (
+        <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains} appInfo={demoAppInfo}>
+                {mounted && children}
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
+}
