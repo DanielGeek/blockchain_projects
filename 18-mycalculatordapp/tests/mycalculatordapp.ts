f81a784977256ee9807dd5a788248c9c12fe042b
Daniel <daniel.passage606@passmail.com>
2024-03-12 16:47:07 -0400

    28. Write Mocha tests for your "addition" function

--- 18-mycalculatordapp/tests/mycalculatordapp.ts
+++ 18-mycalculatordapp/tests/mycalculatordapp.ts
@@ -26,4 +26,14 @@ describe("mycalculatordapp", () => {
     assert.ok(account.greeting === "Welcome to Solana");
   });
 
+  it('Adds two numbers', async() => {
+    await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+      accounts: {
+        calculator: calculator.publicKey
+      }
+    })
+    const account = await program.account.calculator.fetch(calculator.publicKey)
+    assert.ok(account.result.eq(new anchor.BN(5)))
+  })
+
 });
