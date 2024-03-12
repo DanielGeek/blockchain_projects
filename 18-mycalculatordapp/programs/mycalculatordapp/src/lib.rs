7d7b5d321d4b070d0043cabc435b611d61460d2d
Daniel <daniel.passage606@passmail.com>
2024-03-12 16:29:47 -0400

    27. Write the "addition" function for your calculator

--- 18-mycalculatordapp/programs/mycalculatordapp/src/lib.rs
+++ 18-mycalculatordapp/programs/mycalculatordapp/src/lib.rs
@@ -11,6 +11,12 @@ pub mod mycalculatordapp {
       calculator.greeting = init_message;
       Ok(())
     }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+      let calculator = &mut ctx.accounts.calculator;
+      calculator.result = num1 + num2;
+      Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -20,6 +26,12 @@ pub struct Create<'info> {
   #[account(mut)]
   pub user: Signer<'info>,
   pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+  #[account(mut)]
+  pub calculator: Account<'info, Calculator>
 }
 
 #[account]
