8bb7251bbc20c59741ce8b8686a8fbd4bc544144
Daniel <daniel.passage606@passmail.com>
2024-03-15 14:46:02 -0400

    57. Retrieve the Solana object in your web app

--- 20-nft-drop/app/src/App.js
+++ 20-nft-drop/app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 
@@ -7,29 +7,58 @@ const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-	return (
-		<div className="App">
-			<div className="container">
-				<div className="header-container">
-					<p className="header">🍭 Candy Drop</p>
-					<p className="sub-text">NFT drop machine with fair mint</p>
-				</div>
-				<div className="footer-container">
-					<img
-						alt="Twitter Logo"
-						className="twitter-logo"
-						src={twitterLogo}
-					/>
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`Adapted from @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { solana } = window;
+            if (solana) {
+                if (solana.isPhantom) {
+                    console.log("Phantom wallet found!");
+                    const response = await solana.connect({
+                        onlyIfTrusted: true,
+                    });
+                    console.log("Connected with Public Key: ", response.publicKey.toString());
+                }
+            } else {
+                alert("Solana object not found! Get a Phantom Wallet!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        const onLoad = async () => {
+            await checkIfWalletIsConnected();
+        };
+        window.addEventListener("load", onLoad);
+        return () => window.removeEventListener("load", onLoad);
+    }, [])
+    
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header-container">
+                    <p className="header">🍭 Candy Drop</p>
+                    <p className="sub-text">NFT drop machine with fair mint</p>
+                </div>
+                <div className="footer-container">
+                    <img
+                        alt="Twitter Logo"
+                        className="twitter-logo"
+                        src={twitterLogo}
+                    />
+                    <a
+                        className="footer-text"
+                        href={TWITTER_LINK}
+                        target="_blank"
+                        rel="noreferrer"
+                    >{`Adapted from @${TWITTER_HANDLE}`}</a>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
