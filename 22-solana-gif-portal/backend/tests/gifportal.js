afcacbcbf05b09cbed3f474660ac0ac4f617b485
Daniel <daniel.passage606@passmail.com>
2024-03-17 17:45:03 -0400

    75. Store GIF data on the blockchain - part 1

--- 22-solana-gif-portal/backend/tests/gifportal.js
+++ 22-solana-gif-portal/backend/tests/gifportal.js
@@ -7,9 +7,29 @@ const main = async () => {
     anchor.setProvider(provider);
 
     const program = anchor.workspace.Gifportal;
+    const baseAccount = anchor.web3.Keypair.generate();
+    const tx = await program.rpc.startStuffOff({
+        accounts: {
+            baseAccount: baseAccount.publicKey,
+            user: provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [baseAccount]
+    });
 
-    const tx = await program.rpc.startStuffOff();
     console.log("Your transaction signature ", tx);
+
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Gif Count ", account.totalGifs.toString());
+
+    await program.rpc.addGif({
+        accounts: {
+            baseAccount: baseAccount.publicKey,
+        },
+    });
+
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Gif Count ", account.totalGifs.toString());
 };
 
 const runMain = async () => {
