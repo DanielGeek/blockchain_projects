4976e6700a7e8a5d8707a5270cc166a1cc180540
Daniel <daniel.passage606@passmail.com>
2024-03-22 18:02:54 -0400

    Added openzeppelin

--- 26-foundry-defi-stablecoin/README.md
+++ 26-foundry-defi-stablecoin/README.md
@@ -1,3 +1,5 @@
+# This project is meant to be a stablecoin where users can deposit WETH and WBTC in exchange for a token that will be pegged to the USD.
+
 1. Relative Stability: Anchored or Pegged -> $1.00
    1. Chainlink Price feed.
    2. Set a function to exchange ETH & BTC -> $$$
---
+++ 26-foundry-defi-stablecoin/src/DecentralizedStableCoin.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Daniel Ángel
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */  
+contract DecentralizedStableCoin {
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+}
