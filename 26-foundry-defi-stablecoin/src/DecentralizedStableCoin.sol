0e6c512b293a7e2a1ec7df3c89cd27089f6ba3bb
Daniel <daniel.passage606@passmail.com>
2024-03-22 18:34:34 -0400

    Finished DecentralizedStableCoin contract

--- 26-foundry-defi-stablecoin/src/DecentralizedStableCoin.sol
+++ 26-foundry-defi-stablecoin/src/DecentralizedStableCoin.sol
@@ -23,19 +23,47 @@
 // private
 // view & pure functions
 
-pragma solidity ^0.8.18;
+pragma solidity 0.8.20;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title DecentralizedStableCoin
  * @author Daniel Ángel
- * Collateral: Exogenous (ETH & BTC)
- * Minting: Algorithmic
- * Relative Stability: Pegged to USD
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
  *
- * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
- */  
-contract DecentralizedStableCoin {
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+ * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
