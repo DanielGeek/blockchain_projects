e05fe23b9e4825bbe30b953b78bc318f946f0885
Daniel <daniel.passage606@passmail.com>
2024-03-21 22:54:03 -0400

    25. The Hashing Algorithm Part 2, and Lambda Functions

--- 25-python-blockchain/block.py
+++ 25-python-blockchain/block.py
@@ -1,15 +1,20 @@
 import time
 
+from crypto_hash import crypto_hash
+
 class Block:
     """
     Block: a unit of storage.
-    Store transaction in a blockchain that supports a cryptocurrency.
+    Store transactions in a blockchain that supports a cryptocurrency.
     """
     def __init__(self, timestamp, last_hash, hash, data):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+
+    def add_block(self, data):
+        self.chain.append(Block(data))
 
     def __repr__(self):
         return (
@@ -19,7 +24,7 @@ class Block:
             f'hash: {self.hash}, '
             f'data: {self.data})'
         )
-    
+
     @staticmethod
     def mine_block(last_block, data):
         """
@@ -27,7 +32,7 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = f'{timestamp}-{last_hash}'
+        hash = crypto_hash(timestamp, last_hash, data)
 
         return Block(timestamp, last_hash, hash, data)
 
@@ -37,11 +42,12 @@ class Block:
         Generate the genesis block.
         """
         return Block(1, 'genesis_last_hash', 'genesis_hash', [])
-    
+
+
 def main():
     genesis_block = Block.genesis()
     block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    print(f'block: {block}')
 
 if __name__ == '__main__':
     main()
