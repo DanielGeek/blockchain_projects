5ac8a6e87d537cde668e3d088ebfffa2d60e149a
Daniel <daniel.passage606@passmail.com>
2024-03-25 23:43:51 -0400

    Lesson 12: Handler-based Fuzz (Invariant) tests, Redeeming collateral

--- 26-foundry-defi-stablecoin/test/fuzz/Handler.t.sol
+++ 26-foundry-defi-stablecoin/test/fuzz/Handler.t.sol
@@ -39,6 +39,16 @@ contract Handler is Test {
         vm.stopPrank();
     }
 
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateralToRedeem = dsce.getCollateralBalanceOfUser(address(collateral), msg.sender);
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+        if (amountCollateral == 0) {
+            return;
+        }
+        dsce.redeemCollateral(address(collateral), amountCollateral);
+    }
+
     // Helper Functions
     function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock)
     {
