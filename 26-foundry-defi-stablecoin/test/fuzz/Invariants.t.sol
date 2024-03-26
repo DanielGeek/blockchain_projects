b31a009d95ae73bd15ef1a2566949d584e0bb8e1
Daniel <daniel.passage606@passmail.com>
2024-03-25 23:32:33 -0400

    Lesson 12: Handler-based Fuzz (Invariant) tests, Setting revert_on_fail = true

--- 26-foundry-defi-stablecoin/test/fuzz/Invariants.t.sol
+++ 26-foundry-defi-stablecoin/test/fuzz/Invariants.t.sol
@@ -16,6 +16,7 @@ import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
 
 contract Invariants is StdInvariant, Test {
     DeployDSC deployer;
@@ -24,12 +25,16 @@ contract Invariants is StdInvariant, Test {
     HelperConfig config;
     address weth;
     address wbtc;
+    Handler handler;
 
     function setUp() external {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
         (,, weth, wbtc, ) = config.activeNetworkConfig();
-        targetContract(address(dsce));
+        // targetContract(address(dsce));
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+        // hey, don't call redeemcollateral, usless there is collateral to redeem
     }
 
     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
