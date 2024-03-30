7c83f2aaed7e069bce1d0496d8068a1843b3aed6
Daniel <daniel.passage606@passmail.com>
2024-03-29 22:52:04 -0400

    18. Conditionals

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,31 +1,43 @@
-// 16. Functions and Code Blocks
+// 17 Conditionals if, else, etc...
 
 fn main() {
-    my_fn("This is my function!");
-    let str = "Function call with a variable";
-    my_fn(str);
+    let num = 40;
+    if num < 50 {
+        println!("The number is less than 50");
+    } else {
+        println!("The number is greater than or equal to 50");
+    }
 
-    let answer = multiplication(10, 15);
+    let marks = 95;
+    let mut grade = 'N';
 
-    let result = basic_math(10, 15);
-    let (multiplication, addition, substraction) = basic_math(10, 15);
+    // if marks >= 90 {
+    //     grade = 'A';
+    // } else if marks >= 80 {
+    //     grade = 'B';
+    // } else if marks >= 70 {
+    //     grade = 'C';
+    // } else {
+    //     grade = 'F';
+    // }
 
-    let full_name = {
-        let first_name = "Daniel";
-        let last_name = "Angel";
-        format!("{first_name} {last_name}")
+    let grade = if marks >= 90 {
+        'A'
+    } else if marks >= 80 {
+        'B'
+    } else if marks >= 70 {
+        'C'
+    } else {
+        'F'
     };
-}
 
-fn my_fn(s: &str) {
-    println!("{s}");
-}
+    let marks = 95;
+    // let mut grade = 'N';
 
-fn multiplication(num1: i32, num2: i32) -> i32 {
-    println!("Cumputing multiplication");
-    num1 * num2
-}
-
-fn basic_math(num1: i32, num2: i32) -> (i32, i32, i32) {
-    (num1 * num2, num1 + num2, num1 - num2)
+    let grade = match marks {
+        90..=100 => 'A',
+        80..=90 => 'B',
+        70..=79 => 'C',
+        _ => 'F',
+    };
 }
