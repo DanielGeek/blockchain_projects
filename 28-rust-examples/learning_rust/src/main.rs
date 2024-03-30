fc67bb67f57e82dfbf2b9cec29a72176129550c9
Daniel <daniel.passage606@passmail.com>
2024-03-30 19:15:55 -0400

    25. Borrowing Basics

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,36 +1,25 @@
-// 23. Ownership in Functions.
+// 24. Borrowing.
+/*
+    - Borrowing Rules
+        - At any time, you can have either one mutable reference or any number of immutable references.
+        - References must always be valid.
+
+    - Solve out two problems
+        - Data race
+        - Dangling references
+*/
 
 fn main() {
-    let vec_1 = vec![1, 2, 3];
-    takes_ownership(vec_1.clone());
-    println!("vec 1 is: {:?}", vec_1);
+    let mut vec_1 = vec![4, 5, 6];
+    // let ref1 = &mut vec_1;
+    // let ref2 = &mut vec_1;
+    let ref1 = &vec_1;
+    let ref2 = &vec_1;
+    println!("ref1: {:?}, ref2: {:?}", ref1, ref2);
+    let ref3 = &mut vec_1;
 
-    let vec_2 = gives_ownership();
-    println!("vec 2 is: {:?}", vec_2);
-
-    let vec_3 = takes_and_gives_ownership(vec_2);
-    // println!("vec 2 is: {:?}", vec_2);
-    println!("vec 3 is: {:?}", vec_3);
-
-    let x = 10;
-    stack_function(x);
-    println!("In main, x is: {x}");
-}
-
-fn takes_ownership(vec: Vec<i32>) {
-    println!("vec is: {:?}", vec);
-}
-
-fn gives_ownership() -> Vec<i32> {
-    vec![4, 5, 6]
-}
-
-fn takes_and_gives_ownership(mut vec: Vec<i32>) -> Vec<i32> {
-    vec.push(10);
-    vec
-}
-
-fn stack_function(mut var: i32) {
-    var = 56;
-    println!("In func, var is: {var}");
+    let vec_2 = {
+        let vec_3 = vec![1, 2, 3];
+        &vec_3
+    };
 }
