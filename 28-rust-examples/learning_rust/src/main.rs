f95d9b86078e97b3bd8104ca4ad681af21aa07c9
Daniel <daniel.passage606@passmail.com>
2024-03-30 22:46:02 -0400

    29. Structs Basics

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,18 +1,41 @@
-// 28. Dereferencing
+// 29. Structs and its Types
 
+struct Car {
+    owner: String,
+    year: u32,
+    fuel_level: f32,
+    price: u32,
+}
 fn main() {
-    let mut some_data = 42;
-    let ref_1 = &mut some_data;
-    let deref_copy = *ref_1;
-    *ref_1 = 13;
-    println!("some_data is: {some_data}, deref_copy is: {deref_copy}");
+    let mut my_car = Car {
+        owner: String::from("ABC"),
+        year: 2010,
+        fuel_level: 0.0,
+        price: 5_000,
+    };
+    let car_year = my_car.year;
+    my_car.fuel_level = 30.0;
+    let extracted_owner = my_car.owner.clone();
+    println!("Owner is: {}", my_car.owner);
 
-    let mut heap_data = vec![5, 6, 7];
-    let ref_1 = &heap_data;
-    let ref_2 = ref_1;
-    let ref_3 = ref_1;
-    let deref_copy = ref_1.clone();
+    let another_car = Car {
+        owner: "new_name".to_string(),
+        ..my_car
+    };
 
-    let move_out = ref_1;
-    // let move_out_again = ref_1;
+    // println!("Owner is: {}", my_car.owner);
+
+    // Tuple Structs
+    let point_2D = (1, 3);
+    let point_3D = (4, 10, 13);
+
+    struct Point_2d(i32, i32);
+    struct Point_3D(i32, i32, i32);
+
+    let point1 = Point_2d(1, 3);
+    let point2 = Point_3D(4, 10, 13);
+
+    // Unit Struct
+    struct ABC;
+
 }
