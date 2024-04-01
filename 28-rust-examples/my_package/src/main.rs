82e40ad1be01bc4c72228b1615867ff8fdb0b6e0
Daniel <daniel.passage606@passmail.com>
2024-03-31 21:20:21 -0400

    43. Re-exporting and Privacy

--- 28-rust-examples/my_package/src/main.rs
+++ 28-rust-examples/my_package/src/main.rs
@@ -1,5 +1,9 @@
-// 40. Modules
+// 43. Privacy in Modules
 
+use my_package::{Customer, Product, Order, Category};
 fn main() {
-    println!("Hello, world!");
+    let product = Product::new(1, String::from("Laptop"), 799.99, Category::Electronics);
+    let customer = Customer::new(1, String::from("Alice"), String::from("alice@example.com"));
+    let order = Order::new(1, product, customer, 2);
+    println!("Total cost of the order: ${}", order.total_bill());
 }
