9bac39c53b29d97470e1a879b1466c6ad3fc8925
Daniel <daniel.passage606@passmail.com>
2024-04-03 21:44:34 -0400

    57. Traits

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,53 +1,80 @@
-// 55. Generics
+// 57. Traits
 
-struct Point<T, U> {
-    x: T,
-    y: U,
+
+struct drawing_info {
+    line_width: u8,
+    color: String,
+}
+struct Square {
+    side: f32,
+    info: drawing_info,
 }
 
-impl <T, U> Point<T, U> {
-    fn new(x: T, y: U) -> Point<T, U> {
-        Point { x, y }
+struct Rectangle {
+    length: f32,
+    width: f32,
+    info: drawing_info,
+}
+
+// impl Square {
+//     fn calculate_area(&self) {
+//         println!("The area is: {}", self.side * self.side);
+//     }
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> f32 {
+//         self.length * self.width
+//     }
+// }
+
+trait Shape {
+    fn area(&self) -> f32;
+    fn perimeter(&self) -> f32 {
+        println!("Perimeter not implemented, returning dummy value");
+        0.0
     }
 }
 
-impl Point<i32, i32> {
-    fn println(&self) {
-        println!("The values of the coordinates are {}, {}", self.x, self.y);
+impl Shape for Rectangle {
+    fn area(&self) -> f32 {
+        let area_of_rect = self.length * self.width;
+        println!("Rectangle area: {}", area_of_rect);
+        area_of_rect
     }
 
-    fn new_1(x: i32, y: i32) -> Point<i32, i32> {
-        Point { x, y }
-    }
-}
-
-impl Point<f64, f64> {
-    fn println(&self) {
-        println!("The values of the coordinates are {}, {}", self.x, self.y);
+    fn perimeter(&self) -> f32 {
+        let perimeter_of_rect = 2.0 * (self.length + self.width);
+        println!("Rectangle Perimeter: {}", perimeter_of_rect);
+        perimeter_of_rect
     }
 }
 
-fn add_points<T, U>(p1: &Point<T, U>, p2: &Point<T, U>) -> Point<T, U> {
-    unimplemented!();
-}
-
-fn add_points_i32(p1: &Point<i32, i32>, p2: &Point<i32, i32>) -> Point<i32, i32> {
-    unimplemented!();
-}
-
-fn add_points_f64(p1: &Point<f64, f64>, p2: &Point<f64, f64>) -> Point<f64, f64> {
-    unimplemented!();
+impl Shape for Square {
+    fn area(&self) -> f32 {
+        let area_of_square = self.side * self.side;
+        println!("Square area: {}", area_of_square);
+        area_of_square
+    }
 }
 
 fn main() {
-    let origin = Point::new(0, 0);
-    let p1 = Point::new(1.0, 4.0);
+    let r1 = Rectangle {
+        width: 5.0,
+        length: 4.0,
+        line_width: 1,
+        color: String::from("Red"),
+    };
 
-    let p2 = Point::new(5, 5.0);
+    let s1 = Square {
+        side: 3.2,
+        line_width: 1,
+        color: String::from("Red"),
+    };
 
-    origin.println();
+    r1.area();
+    s1.area();
 
-    add_points(&origin, &origin);
-    add_points(&p1, &p1);
-    
+    r1.perimeter();
+    s1.perimeter();
 }
---
+++ 28-rust-examples/learning_rust/src/main19.rs
@@ -0,0 +1,53 @@
+// 55. Generics
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl <T, U> Point<T, U> {
+    fn new(x: T, y: U) -> Point<T, U> {
+        Point { x, y }
+    }
+}
+
+impl Point<i32, i32> {
+    fn println(&self) {
+        println!("The values of the coordinates are {}, {}", self.x, self.y);
+    }
+
+    fn new_1(x: i32, y: i32) -> Point<i32, i32> {
+        Point { x, y }
+    }
+}
+
+impl Point<f64, f64> {
+    fn println(&self) {
+        println!("The values of the coordinates are {}, {}", self.x, self.y);
+    }
+}
+
+fn add_points<T, U>(p1: &Point<T, U>, p2: &Point<T, U>) -> Point<T, U> {
+    unimplemented!();
+}
+
+fn add_points_i32(p1: &Point<i32, i32>, p2: &Point<i32, i32>) -> Point<i32, i32> {
+    unimplemented!();
+}
+
+fn add_points_f64(p1: &Point<f64, f64>, p2: &Point<f64, f64>) -> Point<f64, f64> {
+    unimplemented!();
+}
+
+fn main() {
+    let origin = Point::new(0, 0);
+    let p1 = Point::new(1.0, 4.0);
+
+    let p2 = Point::new(5, 5.0);
+
+    origin.println();
+
+    add_points(&origin, &origin);
+    add_points(&p1, &p1);
+    
+}
