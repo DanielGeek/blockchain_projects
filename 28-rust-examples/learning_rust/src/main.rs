6242dc2651572a8c1dc58d31fbef6f5bc9beb2e4
Daniel <daniel.passage606@passmail.com>
2024-04-06 17:26:38 -0400

    74. IntoIter

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,52 +1,56 @@
-// 73. Iterators
+// 74. IntoIterator
 
-// trait Iterator {
+// trait IntoIterator {
 //     type Item;
-//     fn next(&mut self) -> Option<Self::Item>;
+//     type IntoIter: Iterator;
+//     fn into_iter(self) -> Self::IntoIter;
 // }
 
-struct Employee {
-    name: String,
-    salary: u16,
+struct Book {
+    title: String,
+    author: String,
+    genre: String,
 }
 
-struct Employee_Records {
-    employee_db: Vec<Employee>,
-}
+// struct BookIterator {
+//     properties: Vec<String>
+// }
 
-impl Iterator for Employee_Records {
+// impl Iterator for BookIterator {
+//     type Item = String;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if !self.properties.is_empty() {
+//             Some(self.properties.remove(0))
+//         } else {
+//             None
+//         }
+//     }
+// }
+
+impl IntoIterator for Book {
     type Item = String;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.employee_db.len() != 0 {
-            let result = self.employee_db[0].name.clone();
-            self.employee_db.remove(0);
-            Some(result)
-        } else {
-            None
-        }
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        vec![self.title, self.author, self.genre].into_iter()
     }
 }
+
 fn main() {
-    let mut emp_1 = Employee {
-        name: String::from("John"),
-        salary: 40_000,
+    let book = Book {
+        title: "Digital Image Processing".to_string(),
+        author: "Gonzales".to_string(),
+        genre: "Science Book".to_string(),
     };
 
-    let mut emp_2 = Employee {
-        name: String::from("Joseph"),
-        salary: 30_000,
-    };
+    let mut book_iterator = book.into_iter();
+    // println!("{:?}", book_iterator.next());
+    // println!("{:?}", book_iterator.next());
+    // println!("{:?}", book_iterator.next());
+    // println!("{:?}", book_iterator.next());
 
-    let mut emp_db = Employee_Records {
-        employee_db: vec![emp_1, emp_2],
-    };
-
-    // println!("{:?}", emp_db.next());
-    // println!("{:?}", emp_db.next());
-    // println!("{:?}", emp_db.next());
-
-    for employee in emp_db {
-        println!("{employee}");
+    for book_info in book_iterator {
+        println!("{book_info}");
     }
-
 }
