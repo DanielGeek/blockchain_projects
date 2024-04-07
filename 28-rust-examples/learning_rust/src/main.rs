94eef2b622ac5e930edbdd94cf49699430a16cdb
Daniel <daniel.passage606@passmail.com>
2024-04-07 16:07:52 -0400

    87. Box Smart Pointer (Part 2)

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -16,20 +16,39 @@ enum Conveyance {
 }
 */
 
-#[derive(Debug)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
+// #[derive(Debug)]
+// enum List {
+//     Cons(i32, Option<Box<List>>),
+// }
+// fn main() {
+//     // let x = 0.625;
+//     // let y = Box::new(x);
+//     // let z = &x;
+
+//     let list = List::Cons(
+//         1, 
+//         Some(Box::new(List::Cons(2, Some(Box::new(List::Cons(3, None)))))),
+//     );
+
+//     println!("{:?}", list);
+// }
+
+struct Huge_Data;
+struct Small_Data;
+
+trait Storage {}
+
+impl Storage for Huge_Data {}
+impl Storage for Small_Data {}
+
 fn main() {
-    // let x = 0.625;
-    // let y = Box::new(x);
-    // let z = &x;
+    let data_1 = Huge_Data;
+    let data_2 = Box::new(Huge_Data);
 
-    let list = List::Cons(
-        1, 
-        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
-    );
+    let data_3 = data_1;
+    let data_4 = data_2;
 
-    println!("{:?}", list);
+    let data_5 = Box::new(Small_Data);
+
+    let data: Vec<Box<dyn Storage>> = vec![Box::new(data_3), data_4, data_5];
 }
