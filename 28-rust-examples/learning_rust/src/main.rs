559d22f384451e6274a4efe0b4ff144c5c03b378
Daniel <daniel.passage606@passmail.com>
2024-04-06 20:19:01 -0400

    76. Iterating over Collections

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,56 +1,24 @@
-// 74. IntoIterator
+// 76. Iterating Through Collection
 
-// trait IntoIterator {
-//     type Item;
-//     type IntoIter: Iterator;
-//     fn into_iter(self) -> Self::IntoIter;
-// }
-
-struct Book {
-    title: String,
-    author: String,
-    genre: String,
-}
-
-// struct BookIterator {
-//     properties: Vec<String>
-// }
-
-// impl Iterator for BookIterator {
-//     type Item = String;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if !self.properties.is_empty() {
-//             Some(self.properties.remove(0))
-//         } else {
-//             None
-//         }
-//     }
-// }
-
-impl IntoIterator for Book {
-    type Item = String;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        vec![self.title, self.author, self.genre].into_iter()
-    }
-}
+use std::collections::HashMap;
 
 fn main() {
-    let book = Book {
-        title: "Digital Image Processing".to_string(),
-        author: "Gonzales".to_string(),
-        genre: "Science Book".to_string(),
-    };
+    let mut vec_1 = vec![45, 30, 85, 90, 41, 39];
+    // let mut vec_1_iter = vec_1.into_iter();
+    // let value_1 = vec_1_iter.next();
 
-    let mut book_iterator = book.into_iter();
-    // println!("{:?}", book_iterator.next());
-    // println!("{:?}", book_iterator.next());
-    // println!("{:?}", book_iterator.next());
-    // println!("{:?}", book_iterator.next());
+    for values in vec_1 {
+        println!("{values}");
+    }
 
-    for book_info in book_iterator {
-        println!("{book_info}");
+    // println!("{:?}", vec_1);
+
+    let mut person: HashMap<String, i32> = HashMap::new();
+    person.insert("Hannash".to_string(), 40);
+    person.insert("Joseph".to_string(), 44);
+    person.insert("Sara".to_string(), 55);
+
+    for (name, age) in person {
+        println!("The person {} has an age of {}", name, age);
     }
 }
