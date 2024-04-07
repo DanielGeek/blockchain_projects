4366ea09f5ccc1910e258e79ffd0b1cc130d28fa
Daniel <daniel.passage606@passmail.com>
2024-04-06 21:17:09 -0400

    79. Bonus - Iterating Through Option

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,22 +1,43 @@
-// 77. Combinators
+// 79. Iterating Through Options
 
 fn main() {
-    let words = vec!["apple", "banana", "grape", "orange", "pear"];
-    // let mut result: Vec<String> = vec![];
+    let some_product = Some("laptop");
+    let mut products = vec!["cellphone", "battery", "charger"];
 
-    // for word in words {
-    //     if word.starts_with("a") || word.starts_with("b") {
-    //         let uppercase_word = word.to_uppercase();
-    //         result.push(uppercase_word);
+    // match some_product {
+    //     Some(product) => products.push(product),
+    //     _ => {}
+    // };
+
+    // if let Some(product) = some_product {
+    //     products.push(product);
+    // }
+
+    // products.extend(some_product);
+    // println!("{:?}", products);
+
+    // let products_iter = products.iter().chain(some_product.iter());
+
+    // for prod in products_iter {
+    //     println!("{:?}", prod);
+    // }
+
+    let products = vec![Some("charger"), Some("battery"), None, Some("cellphone")];
+
+    // let mut prod_without_none = Vec::new();
+    // for p in products {
+    //     if p.is_some() {
+    //         prod_without_none.push(p.unwrap());
     //     }
     // }
-    // println!("Result: {:?}", result);
 
-    let result: Vec<String> = words
-        .into_iter()
-        .filter(|&word |word.starts_with("a") || word.starts_with("b"))
-        .map(|word | word.to_uppercase())
-        .collect();
-    println!("Result: {:?}", result);
+    // let prod_without_none = products
+    //     .into_iter()
+    //     .filter(|x | x.is_some())
+    //     .map(|x | x.unwrap())
+    //     .collect::<Vec<&str>>();
+
+    let prod_without_none = products.into_iter().flatten().collect::<Vec<&str>>();
+    println!("{:?}", prod_without_none);
 }
 
