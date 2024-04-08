99e8fe0e73d8c41f24ce531c9491bb4bf84943e0
Daniel <daniel.passage606@passmail.com>
2024-04-07 23:47:19 -0400

    95. Singly Linked List (Part 2)

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,4 +1,4 @@
-// 94. Link List (Part 1)
+// 94. Link List (Part 2)
 
 #[derive(Debug)]
 struct LinkList {
@@ -13,51 +13,66 @@ struct Node {
 
 type pointer = Option<Box<Node>>;
 
+impl LinkList {
+    fn new() -> LinkList {
+        LinkList { head: None }
+    }
+
+    fn add(&mut self, element: i32) {
+        // match self.head {
+        //     None => {
+        //         let new_node = Some(Box::new(Node {
+        //             element: element,
+        //             next: None,
+        //         }));
+        //         self.head = new_node;
+        //     }
+        //     Some(Previous_head) => {
+        //         let new_node = Some(Box::new(Node {
+        //             element: element,
+        //             next: Some(Previous_head),
+        //         }));
+        //         self.head = new_node;
+        //     }
+        // }
+
+        // fn take<T>(dest: &mut T) -> T
+        let previous_head = self.head.take();
+        let new_head = Some(Box::new(Node {
+            element: element,
+            next: previous_head,
+        }));
+        self.head = new_head;
+    }
+
+    fn remove(&mut self) -> Option<i32> {
+        match self.head.take() {
+            Some(previous_head) => {
+                self.head = previous_head.next;
+                Some(previous_head.element)
+            }
+            None => None,
+        }
+    }
+
+    fn print(&self) {
+        let mut list_traversal = &self.head;
+        while !list_traversal.is_none() {
+            println!("{:?}", list_traversal.as_ref().unwrap().element);
+            list_traversal = &list_traversal.as_ref().unwrap().next;
+        }
+    }
+}
+
 fn main() {
-    // let list = Node {
-    //     element: 1,
-    //     next: None,
-    // };
+    let mut list = LinkList::new();
+    list.add(5);
+    list.add(7);
+    list.add(10);
+    list.add(15);
+    list.add(20);
 
-    // let list = Node {
-    //     element: 1,
-    //     next: Some(Box::new(Node {
-    //         element: 2,
-    //         next: None,
-    //     })),
-    // };
-
-    // let list = LinkList {
-    //     head: Some(Node {
-    //         element: 1,
-    //         next: None,
-    //     }),
-    // };
-
-    // let list = LinkList {
-    //     head: Some(Node {
-    //         element: 1,
-    //         next: Some(Box::new(Node {
-    //             element: 2,
-    //             next: Some(Box::new(Node {
-    //                 element: 3,
-    //                 next: None,
-    //             })),
-    //         })),
-    //     }),
-    // };
-
-    // let list = LinkList { head: None };
-
-    let list = LinkList {
-        head: Some(Box::new(Node {
-            element: 100,
-            next: Some(Box::new(Node {
-                element: 200,
-                next: None,
-            })),
-        })),
-    };
-
-    println!("{:?}", &list.head);
+    // println!("List: {:?}", list);
+    list.print();
+    println!("{}", list.remove().unwrap());
 }
---
+++ 28-rust-examples/learning_rust/src/main39.rs
@@ -0,0 +1,63 @@
+// 94. Link List (Part 1)
+
+#[derive(Debug)]
+struct LinkList {
+    head: pointer,
+}
+
+#[derive(Debug)]
+struct Node {
+    element: i32,
+    next: pointer,
+}
+
+type pointer = Option<Box<Node>>;
+
+fn main() {
+    // let list = Node {
+    //     element: 1,
+    //     next: None,
+    // };
+
+    // let list = Node {
+    //     element: 1,
+    //     next: Some(Box::new(Node {
+    //         element: 2,
+    //         next: None,
+    //     })),
+    // };
+
+    // let list = LinkList {
+    //     head: Some(Node {
+    //         element: 1,
+    //         next: None,
+    //     }),
+    // };
+
+    // let list = LinkList {
+    //     head: Some(Node {
+    //         element: 1,
+    //         next: Some(Box::new(Node {
+    //             element: 2,
+    //             next: Some(Box::new(Node {
+    //                 element: 3,
+    //                 next: None,
+    //             })),
+    //         })),
+    //     }),
+    // };
+
+    // let list = LinkList { head: None };
+
+    let list = LinkList {
+        head: Some(Box::new(Node {
+            element: 100,
+            next: Some(Box::new(Node {
+                element: 200,
+                next: None,
+            })),
+        })),
+    };
+
+    println!("{:?}", &list.head);
+}
