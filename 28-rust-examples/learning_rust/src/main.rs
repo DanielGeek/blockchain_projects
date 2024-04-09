21bdeca4217582f463fc1dcfbea48af0937ac4f5
Daniel <daniel.passage606@passmail.com>
2024-04-09 18:15:09 -0400

    103. Simplifying Structs

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,130 +1,33 @@
-// 102. Builder Pattern
+// 103. Simplifying Structs
 
-#[derive(Debug, Default, Clone)]
-struct Customer {
-    name: String,
-    username: String,
-    membership: Membershiptype,
-    gender: char,
-    country: String,
-    age: u8,
+struct A {
+    b: B,
+    c: C,
 }
 
-#[derive(Debug, Clone)]
-enum Membershiptype {
-    new,
-    causual,
-    loyal,
+struct B {
+    f2: u32,
 }
 
-impl Default for Membershiptype {
-    fn default() -> Self {
-        Membershiptype::new
-    }
+struct C {
+    f1: u32,
+    f3: u32,
 }
 
-impl Customer {
-    fn new(name: String) -> CustomerBuilder {
-        CustomerBuilder {
-            name: name,
-            ..Default::default()
-            // username: None,
-            // membership: None,
-            // gender: None,
-            // country: None,
-            // age: None
-        }
-    }
-    // fn new(name: String) -> Self {
-    //     Customer {
-    //         name: name,
-    //         ..Default::default()
-    //     }
-    // }
-
-    // fn new_2(name: String, username: String) -> Self {
-    //     Customer {
-    //         name: name,
-    //         username: username,
-    //         ..Default::default()
-    //     }
-    // }
-
-    // fn new_3(name: String, username: String, membership: Membershiptype) -> Self {
-    //     Customer {
-    //         name: name,
-    //         username: username,
-    //         membership: membership,
-    //         ..Default::default()
-    //     }
-    // }
+fn fn1(a: &mut B) -> &u32 {
+    &a.f2
 }
 
-#[derive(Default)]
-struct CustomerBuilder {
-    name: String,
-    username: Option<String>,
-    membership: Option<Membershiptype>,
-    gender: Option<char>,
-    country: Option<String>,
-    age: Option<u8>,
+fn fn2(a: &mut C) -> u32 {
+    a.f1 + a.f3
 }
 
-impl CustomerBuilder {
-    fn username(&mut self, username: String) -> &mut Self {
-        self.username = Some(username);
-        self
-    }
-
-    fn membership(&mut self, membership: Membershiptype) -> &mut Self {
-        self.membership = Some(membership);
-        self
-    }
-
-    fn gender(&mut self, gender: char) -> &mut Self {
-        self.gender = Some(gender);
-        self
-    }
-
-    fn country(&mut self, country: String) -> &mut Self {
-        self.country = Some(country);
-        self
-    }
-
-    fn age(&mut self, age: u8) -> &mut Self {
-        self.age = Some(age);
-        self
-    }
-
-    fn build(&mut self) -> Customer {
-        Customer {
-            name: self.name.clone(),
-            username: self.username.clone().unwrap_or_default(),
-            membership: self.membership.clone().unwrap_or_default(),
-            gender: self.gender.unwrap_or_default(),
-            country: self.country.clone().unwrap_or_default(),
-            age: self.age.unwrap_or_default(),
-        }
-    }
+fn fn3(a: &mut A) {
+    let x = fn1(&mut a.b);
+    let y = fn2(&mut a.c);
+    println!("{x}");
 }
 
 fn main() {
-    // let new_user = Customer::new("Nouman".to_string());
-    // let user_with_login = Customer::new_2("Joseph".to_string(), "joe123".to_string());
-    // let user_with_membership = Customer::new_3(
-    //     "Micheal".to_string(),
-    //     "micheal2000".to_string(),
-    //     Membershiptype::loyal,
-    // );
-
-    let new_user = Customer::new("Nouman".to_string()).build();
-    let user_with_login = Customer::new("Joseph".to_string())
-        .username("joe123".to_string())
-        .build();
-
-    let user_with_membership = Customer::new("Micheal".to_string())
-        .username("micheal2000".to_string())
-        .membership(Membershiptype::loyal)
-        .build();
-
+    
 }
