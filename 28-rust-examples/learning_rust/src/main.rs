47d4e156ab51a7a81a3888dc56b8d4073f47f6fb
Daniel <daniel.passage606@passmail.com>
2024-04-11 17:53:05 -0400

    113. Zero Sized Phantom Data

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,45 +1,19 @@
-// 112. - Zero Sized Types
-//          - Unit Struct
+// 113. - Zero Sized Types
+//          - Phantom Data
 
-struct Admin;
-struct User;
-trait Authenticate {
-    fn authenticate(&self, username: &str, password: &str) -> bool;
-}
+// use negative_impl::negative_impl;
 
-impl Authenticate for Admin {
-    fn authenticate(&self, username: &str, password: &str) -> bool {
-        username == "admin" && password == "adminpass"
-    }
-}
+// #[negative_impl]
+// impl !Send for ABC {}
 
-impl Authenticate for User {
-    fn authenticate(&self, username: &str, password: &str) -> bool {
-        username == "user" && password == "userpass"
-    }
-}
+// #[negative_impl]
+// impl !Sync for ABC {}
 
-fn login<T: Authenticate>(role: T, username: &str, password: &str) -> bool {
-    role.authenticate(username, password)
+use std::{marker::PhantomData, mem::size_of, rc::Rc};
+
+struct ABC {
+    ensuring_no_send_sync: PhantomData<Rc<()>>,
 }
 fn main() {
-    let admin = Admin;
-    let user = User;
-    let admin_login = login(admin, "admin", "adminpass");
-    let user_login = login(user, "user", "userpass");
-
-    if admin_login {
-        println!("Admin login successful!");
-    } else {
-        println!("User login successful!");
-    }
-
-    let x = ();
-    let y = x;
-    let z = x;
-
-    struct ABC;
-    let x = ABC;
-    let y = x;
-    let z = x;
+    println!("{}", size_of::<ABC>());
 }
