46c0ff632cf99aaf8bcd78a55d472141c7b5b1fa
Daniel <daniel.passage606@passmail.com>
2024-04-20 18:08:27 -0400

    118. Multiple Producers, Threads and Functions

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,33 +1,69 @@
-// 117. - Message Passing through Channels
 
-use std::thread;
-use std::sync::mpsc;
-fn main() {
-    let (tx, rx) = mpsc::channel();
+// Sending Multiple Messages
+// Multitple Producers
+// Threads and Functions
 
-    // let rx1 = rx;
+// use std::{sync::mpsc, thread};
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//     let t = thread::spawn(move || {
+//         let my_vec = vec![1,2,3,4,5];
+//         for i in my_vec {
+//             tx.send(i).unwrap();
+//         }
+//     });
 
-    let t = thread::spawn(move || {
-        let val = String::from("Some data from sender");
-        println!("Value sending from the thread");
-        tx.send(val).unwrap();
-        // println!("This may execute after the statement in the main");
-        // println!("Val is {:?}", val);
+//     // for recieved_vals in rx {
+//     //     println!("I recieved the value of {}", recieved_vals);
+//     // }
+
+//     let recieved_vals_vec = rx.iter().collect::<Vec<i32>>();
+//     println!("The recieved values are {:?}", recieved_vals_vec);
+// }
+
+// use std::{thread, sync::mpsc, time::Duration};
+
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//     let tx1 = tx.clone();
+//     thread::spawn(move || {
+//         let my_vec = vec![1,2,3,4,5];
+//         for i in my_vec {
+//             tx.send(i).unwrap();
+//             thread::sleep(Duration::from_secs(1));
+//         }
+//     });
+    
+//     thread::spawn(move || {
+//         let my_vec = vec![6,7,8,9,10];
+//         for i in my_vec {
+//             tx1.send(i).unwrap();
+//             thread::sleep(Duration::from_secs(1));
+//         }
+//     });
+
+//     for recieved_vals in rx {
+//         println!("I recieved the value of {}", recieved_vals);
+//     }
+// }
+
+use std::{thread, sync::mpsc, time::Duration};
+
+fn timer(d: i32, tx: mpsc::Sender<i32>) {
+    thread::spawn(move || {
+        println!("{} send! ", d);
+        tx.send(d).unwrap();
     });
-
-    // let recieved = rx.recv().unwrap();
-    // println!("Recieved: {:?}", recieved);
-
-    t.join();
-    let mut recieved_status = false;
-    while recieved_status != true {
-        match rx.try_recv() {
-            Ok(recieved_value) => {
-                println!("Recieved value is {:?}", recieved_value);
-                recieved_status = true;
-            },
-            Err(_) => println!("I am doing some other stuff"),
-        }
-    }
 }
 
+fn main () {
+    let (tx, rx) = mpsc::channel();
+    for i in 0..5 {
+        timer(i, tx.clone());
+    }
+    drop(tx);
+
+    for recieved_val in rx {
+        println!("{} recieved!", recieved_val);
+    }
+}
