4112cc0897a6f833887ad7a95cf27ab6cc6d4e91
Daniel <daniel.passage606@passmail.com>
2024-04-21 00:33:28 -0400

    122. Scoped Threads

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,62 +1,29 @@
 
-// Passes Mutexes between Threads
+// Scope Threads
 
-// use std::{sync::{Arc, Barrier}, thread};
-
-// fn main() {
-//     let mut threads = Vec::new();
-//     let barrier = Arc::new(Barrier::new(5));
-
-//     for i in 0..3 {
-//         let barrier = barrier.clone();
-//         let t = thread::spawn(move || {
-//             println!("Before wait {}", i);
-//             barrier.wait();
-//             println!("After wait {}", i);
-//         });
-//         threads.push(t);
-//     }
-
-//     for t in threads {
-//         t.join().unwrap();
-//     }
-// }
-
-use std::sync::{Arc, Mutex, Barrier};
 use std::thread;
 
 fn main() {
-    let mut threads = Vec::new();
-    let barrier = Arc::new(Barrier::new(3));
-    let data = Arc::new(vec![
-        vec![1,2,3,4,5,6],
-        vec![1,2,3,4,5,6],
-        vec![1,2,3,4,5,6],
-    ]);
+    let mut vec = vec![1,2,3];
+    let mut x = 0;
 
-    let result = Arc::new(Mutex::new(0));
-    for i in 0..3 {
-        let barrier = barrier.clone();
-        let data = data.clone();
-        let result = result.clone();
-        let t = thread::spawn(move || {
-
-            let mut x = result.lock().unwrap();
-            *x = data[i][0..3].iter().sum();
-            // let x: i32 = data[i][0..3].iter().sum();
-            // *result.lock().unwrap() += x;
-            println!("Thread {} Part 1 is done", i);
-            barrier.wait();
-
-            // let x:i32 = data[i][3..6].iter().sum();
-            // *result.lock().unwrap() += x;
-            *x = data[i][3..6].iter().sum();
-            println!("Thread {} is complete", i);
+    // thread::scope(|some_scope| {
+    //     some_scope.spawn(|| {
+        thread::spawn(|| {
+            println!("I am first thread in the scope");
+            println!("{:?}", vec);
         });
-        threads.push(t);
-    }
-    for t in threads {
-        t.join().unwrap();
-    }
-    println!("The final value of the result is {}", *result.lock().unwrap());
+
+        // some_scope.spawn(|| {
+            thread::spawn(||{
+            println!("I am second thread in the scope");
+            x += 45;
+            // vec.push(4);
+            println!("{:?}", vec);
+        });
+    // });
+
+    println!("The threads are now complete");
+    vec.push(5);
+    println!("x: {:?} and vec: {:?}", x, vec);
 }
