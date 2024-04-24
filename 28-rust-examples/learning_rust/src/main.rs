839a076a059c3c969eb8a2eb4e517011a00617f7
Daniel <daniel.passage606@passmail.com>
2024-04-23 22:20:05 -0400

    151. Directory and Path Related Functions

--- 28-rust-examples/learning_rust/src/main.rs
+++ 28-rust-examples/learning_rust/src/main.rs
@@ -1,50 +1,25 @@
-// Basic File Handling
+// Directory and Path Related Functions
 
-use std::fmt::format;
-use std::fs::*;
-use std::io::{BufRead, BufReader, Read, Write};
-use std::path::Path;
-
-
-fn basic_file_handling() -> std::io::Result<()> {
-    let path_loc = r"/Users/danielgeek/Documents/GitHub/blockchain_projects/28-rust-examples/learning_rust/my_text.txt";
-    let path = Path::new(path_loc);
-    // let mut file = File::create(path)?;
-
-    // file.write(b"let's put this in the file")?;
-    // file.write("let's put this in the file".as_bytes())?;
-
-    // let mut file = OpenOptions::new().append(true).open(path)?;
-    // file.write("\n www.includehelp.com\n".as_bytes());
-
-    // let str1 = "daniel";
-    // file.write(str1.as_bytes())?;
-
-    // let some_vec = vec![1,2,3,4,5,6];
-    // let str_from_vec = some_vec
-    // .into_iter()
-    // .map(|a | format!("{} \n", a.to_string()))
-    // .collect::<String>();
-
-    // file.write(str_from_vec.as_bytes())?;
-
-    // let (name, age) = ("Daniel", 33);
-    // let formatted_str = format!("I am {} and my name is {}", name, age);
-    // file.write(formatted_str.as_bytes());
-
-    // let mut file = File::open(path)?;
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents);
-    // println!("The file contains {:?}", contents);
-
-    let mut file = File::open(path)?;
-    let file_buffer = BufReader::new(file);
-    for lines in file_buffer.lines() {
-        println!("{:?}", lines);
-    }
-    Ok(())
-}
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 fn main(){
-    basic_file_handling();
+    let path = Path::new(r"/Users/danielgeek/Documents/GitHub/blockchain_projects/28-rust-examples/learning_rust/my_text.txt");
+    println!("Folder containing the file: {:?}", path.parent().unwrap());
+
+    println!("Name of the hte file is {:?}", path.file_stem().unwrap());
+    println!("Extention of the file is {:?}", path.extension().unwrap());
+
+    let mut path = PathBuf::from(r"/Users/");
+    path.push(r"danielgeek");
+    path.push(r"Documents");
+    path.push(r"GitHub");
+    path.push(r"blockchain_projects");
+    path.push(r"28-rust-examples");
+    path.push(r"learning_rust");
+    path.push(r"my_text");
+
+    path.set_extension("txt");
+    println!("The path is {:?}", path);
 }
---
+++ 28-rust-examples/learning_rust/src/main83.rs
@@ -0,0 +1,50 @@
+// Basic File Handling
+
+use std::fmt::format;
+use std::fs::*;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::path::Path;
+
+
+fn basic_file_handling() -> std::io::Result<()> {
+    let path_loc = r"/Users/danielgeek/Documents/GitHub/blockchain_projects/28-rust-examples/learning_rust/my_text.txt";
+    let path = Path::new(path_loc);
+    // let mut file = File::create(path)?;
+
+    // file.write(b"let's put this in the file")?;
+    // file.write("let's put this in the file".as_bytes())?;
+
+    // let mut file = OpenOptions::new().append(true).open(path)?;
+    // file.write("\n www.includehelp.com\n".as_bytes());
+
+    // let str1 = "daniel";
+    // file.write(str1.as_bytes())?;
+
+    // let some_vec = vec![1,2,3,4,5,6];
+    // let str_from_vec = some_vec
+    // .into_iter()
+    // .map(|a | format!("{} \n", a.to_string()))
+    // .collect::<String>();
+
+    // file.write(str_from_vec.as_bytes())?;
+
+    // let (name, age) = ("Daniel", 33);
+    // let formatted_str = format!("I am {} and my name is {}", name, age);
+    // file.write(formatted_str.as_bytes());
+
+    // let mut file = File::open(path)?;
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents);
+    // println!("The file contains {:?}", contents);
+
+    let mut file = File::open(path)?;
+    let file_buffer = BufReader::new(file);
+    for lines in file_buffer.lines() {
+        println!("{:?}", lines);
+    }
+    Ok(())
+}
+
+fn main(){
+    basic_file_handling();
+}
