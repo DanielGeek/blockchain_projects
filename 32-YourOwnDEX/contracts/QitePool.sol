15917efe1693506618a7720b64c836df47187f39
Daniel <daniel.passage606@passmail.com>
2024-04-27 23:33:03 -0400

    20. Define the state variable

--- 32-YourOwnDEX/contracts/QitePool.sol
+++ 32-YourOwnDEX/contracts/QitePool.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
+import "./QiteLiquidityToken.sol";
 
 contract QitePool {
+    address public token1;
+    address public token2;
+
+    uint256 public reserve1;
+    uint256 public reserve2;
+
+    // x * y = k
+    uint256 public constantK;
+
+    QiteLiquidityToken public liquidityToken;
 
 }
