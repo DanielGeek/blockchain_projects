b9854498bf63c1775a7d337ba30c00bc13b9075f
Daniel <daniel.passage606@passmail.com>
2024-04-27 23:06:26 -0400

    19. Implement the function create liquidity pool

---
+++ 32-YourOwnDEX/contracts/QitePool.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+
+
+contract QitePool {
+
+}
--- 32-YourOwnDEX/contracts/QiteSwap.sol
+++ 32-YourOwnDEX/contracts/QiteSwap.sol
@@ -1,8 +1,33 @@
 // SPDX-License-Identifier: MIT
 
+import "./QitePool.sol";
 
 contract QiteSwap {
 
     address[] public allPairs;
-    // mapping(address => mapping(address => QitePool)) public getPair;
+    mapping(address => mapping(address => QitePool)) public getPair;
+    event PairCreated(address indexed token1, address indexed token2, address pair);
+
+    function createPairs(address token1, address token2, string calldata token1Name, string calldata token2Name) external returns (address) {
+        require(token1 != token2, "Identical token address is not allowed");
+        require(address(getPair[token1][token2]) == address(0), "Pair already exists");
+
+        QitePool qitePool = new QitePool();
+
+        getPair[token1][token2] = qitePool;
+        getPair[token2][token1] = qitePool;
+        allPairs.push(address(qitePool));
+
+        emit PairCreated(token1, token2, address(qitePool));
+
+        return address(qitePool);
+    }
+
+    function allPairsLength() external view returns(uint) {
+        return allPairs.length;
+    }
+
+    function getPairs() external view returns (address[] memory) {
+        return allPairs;
+    }
 }
