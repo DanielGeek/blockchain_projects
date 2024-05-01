9bea0098ce8c748df5230877854e753d8187403d
Daniel <daniel.passage606@passmail.com>
2024-05-01 17:39:07 -0400

    29. Implement connect to wallet

--- 32-YourOwnDEX/frontend/components/Header.js
+++ 32-YourOwnDEX/frontend/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row">
             <h1 className="py-2 px-8 font-bold text-3xl text-white">
-                Base Front Web Project
+                My Dex Front End
             </h1>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton />
