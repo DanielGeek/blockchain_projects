df58899174e16fda368a3cfae93fa9ffb67f8964
Daniel <daniel.passage606@passmail.com>
2024-06-23 13:23:14 -0400

    Added create wallet code

--- 48-build-a-video-game-marketplace-blockchain/generate-wallet.js
+++ 48-build-a-video-game-marketplace-blockchain/generate-wallet.js
@@ -10,3 +10,16 @@ const ec = new EC.ec('p192');
 
 const newWalletName = process.argv[2];
 // Add your code below
+
+const keyPair = ec.genKeyPair();
+const pub = keyPair.getPublic('hex');
+const priv = keyPair.getPrivate('hex');
+
+const wallets = getWallets();
+
+wallets[newWalletName] = {
+    publicKey:pub,
+    privateKey:priv
+};
+
+writeWallets(wallets);
--- 48-build-a-video-game-marketplace-blockchain/init-blockchain.js
+++ 48-build-a-video-game-marketplace-blockchain/init-blockchain.js
@@ -1,2 +1,10 @@
 import { writeBlockchain, writeTransactions } from './blockchain-helpers.js';
 // Add your code below
+
+const genesisBlock = {
+    hash: "0",
+    previousHash: null
+}
+
+writeBlockchain([ genesisBlock ]);
+writeTransactions([]);
