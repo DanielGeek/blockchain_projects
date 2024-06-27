908b8f9fa5e4c739baeb174b7e32d60b4e09a236
Daniel <daniel.passage606@passmail.com>
2024-06-26 23:39:07 -0400

    Finished buy-items and sell-items functions

--- 48-build-a-video-game-marketplace-blockchain/buy-item.js
+++ 48-build-a-video-game-marketplace-blockchain/buy-item.js
@@ -11,3 +11,24 @@ const ec = new EC.ec('p192');
 const buyerPrivateKey = process.argv[2];
 const itemBought = process.argv[3];
 // Add your code below
+
+const keyPair = ec.keyFromPrivate(buyerPrivateKey);
+const publicKey = keyPair.getPublic('hex');
+
+const price = getItemPrice(itemBought);
+const signature = keyPair.sign(publicKey + price + itemBought).toDER('hex');
+
+const transactions = getTransactions();
+const newTransaction = {
+    buyerAddress: publicKey,
+    sellerAddress: null,
+    price,
+    itemBought,
+    signature
+};
+
+const balance = getAddressBalance(publicKey);
+if (balance >= price) {
+    transactions.push(newTransaction);
+    writeTransactions(transactions);
+}
--- 48-build-a-video-game-marketplace-blockchain/sell-item.js
+++ 48-build-a-video-game-marketplace-blockchain/sell-item.js
@@ -11,3 +11,24 @@ const ec = new EC.ec('p192');
 const sellerPrivateKey = process.argv[2];
 const itemSold = process.argv[3];
 // Add your code below
+
+const transactions = getTransactions();
+const price = getItemPrice(itemSold) - 5;
+
+const keyPair = ec.keyFromPrivate(sellerPrivateKey);
+const publicKey = keyPair.getPublic('hex');
+const items = getAddressItems(publicKey);
+const signature = keyPair.sign(publicKey + price + itemSold).toDER('hex');
+
+const newTransaction = {
+    buyerAddress: null,
+    sellerAddress: publicKey,
+    price,
+    itemSold,
+    signature
+};
+
+if(items[itemSold] >= 1) {
+    transactions.push(newTransaction);
+    writeTransactions(transactions);
+}
