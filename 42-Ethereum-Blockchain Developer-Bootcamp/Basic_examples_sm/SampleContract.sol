d5c8cc310b0ef7b5825b6c389c5541b5040c8c6b
Daniel <daniel.passage606@passmail.com>
2024-06-29 23:11:57 -0400

    [LAB] The Payable Modifier And Msg.value

--- 42-Ethereum-Blockchain Developer-Bootcamp/Basic_examples_sm/SampleContract.sol
+++ 42-Ethereum-Blockchain Developer-Bootcamp/Basic_examples_sm/SampleContract.sol
@@ -5,7 +5,11 @@ pragma solidity 0.8.15;
 contract SampleContract {
     string public myString = "Hello World";
 
-    function updateString(string memory _newString) public {
-        myString = _newString;
+    function updateString(string memory _newString) public payable {
+        if(msg.value == 1 ether) {
+            myString = _newString;
+        } else {
+            payable(msg.sender).transfer(msg.value);
+        }
     }
 }
