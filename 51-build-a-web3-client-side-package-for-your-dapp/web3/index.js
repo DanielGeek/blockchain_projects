ee1d9a90ad7d41af3b071a8a0ebe8b0980d331ca
Daniel <daniel.passage606@passmail.com>
2024-07-03 23:31:11 -0400

    Finished Web3 class

--- 51-build-a-web3-client-side-package-for-your-dapp/web3/index.js
+++ 51-build-a-web3-client-side-package-for-your-dapp/web3/index.js
@@ -1,1 +1,104 @@
-export default class Web3 {}
+export default class Web3 {
+    constructor(provider) {
+        this.address = null;
+        this.provider = new URL(provider);
+    }
+
+    setClientAddress(address) {
+        this.address = address;
+    }
+
+    async call(rpcCall) {
+
+        const body = {
+            ...rpcCall,
+            address: this.address
+        };
+
+        const res = await fetch(`${this.provider.href}call-smart-contract`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        const response = await res.json();
+        if (response.error) {
+            throw new Error(response.error);
+        }
+
+        return response.result;
+    }
+
+    initSmartContract(idl) {
+        const smartContract = {};
+        for (const instruction of idl.instructions) {
+            smartContract[instruction.handle] = async (...args) => {
+                const rpcCall = {
+                    id: idl.id,
+                    method: instruction.handle,
+                    args,
+                }
+                return await this.call(rpcCall);
+            }
+        }
+        return smartContract;
+    }
+
+    async getBalance(address) {
+        const body = {
+            address: address || this.address
+        };
+
+        const res = await fetch(`${this.provider.href}get-balance`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        const response = await res.json();
+        if (response.error) {
+            throw new Error(response.error);
+        }
+
+        return response.result;
+    }
+
+    async transfer({from, to, amount}) {
+        const body = {
+            from: from || this.address, to, amount
+        };
+
+        const res = await fetch(`${this.provider.href}transfer`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        const response = await res.json();
+        if (response.error) {
+            throw new Error(response.error);
+        }
+
+        return response.result;
+    }
+}
+
+// const web3 = new Web3();
+// console.log(web3);
+
+// web3.setClientAddress('Daniel');
+// console.log(web3.address);
+
+// const res = await web3.call({
+//     id: 0,
+//     args: [],
+//     method: 'test',
+// });
+
+// console.log(res);
