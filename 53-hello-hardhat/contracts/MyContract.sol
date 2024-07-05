295a18f3d6126fec1f53823e47d4ba85212c2342
Daniel <daniel.passage606@passmail.com>
2024-07-04 23:57:31 -0400

    NFT Console Log in Solidity with Hardhat

--- 53-hello-hardhat/contracts/MyContract.sol
+++ 53-hello-hardhat/contracts/MyContract.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+import "hardhat/console.sol";
+
 contract MyContract is ERC721 {
     
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol) {
 
+            console.log("name: ", name);
+            console.log("This is the NFT symbol: ", symbol);
+            console.log("msg.sender: ", msg.sender);
     }
 }
