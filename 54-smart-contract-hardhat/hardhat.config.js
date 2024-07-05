6f1dc0891b5b37bc43abb40ee49ec1292699d160
Daniel <daniel.passage606@passmail.com>
2024-07-05 15:32:45 -0400

    finished example

--- 54-smart-contract-hardhat/hardhat.config.js
+++ 54-smart-contract-hardhat/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_MAINNET_RPC_URL,
+      }
+    }
+  }
 };
