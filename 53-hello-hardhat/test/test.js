daec24aa2a248501062fc3919302a228c9eb7112
Daniel <daniel.passage606@passmail.com>
2024-07-04 23:40:18 -0400

    Test NFT Contract with Hardhat, Waffle and Ether

---
+++ 53-hello-hardhat/test/test.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+
+describe("MyContract", function() {
+
+    it("should return correct name and symbol", async function () {
+
+        const MyContract = await hre.ethers.getContractFactory("MyContract");
+
+        const myContractDeployed = await MyContract.deploy("MyContractName", "MCN");
+
+        await myContractDeployed.deployed();
+
+        expect(await myContractDeployed.name()).to.equal("MyContractName");
+        expect(await myContractDeployed.symbol()).to.equal("MCN");
+    });
+});
