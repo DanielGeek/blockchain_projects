5b57d3c2a9708cf59377ffd9c9366c354b4f70f4
Daniel <daniel.passage606@passmail.com>
2024-07-05 20:23:37 -0400

    setup ethereum MinimalAccount SM

---
+++ 56-foundry-account-abstraction/src/ethereum/MinimalAccount.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+contract MinimalAccount is IAccount, Ownable {
+    IEntryPoint private immutable i_entryPoint;
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+    
+    // A signature is valid, if it's the MinimalAccount owner
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds) 
+        external
+        returns (uint256 validationData)
+        {
+            validationData = _validateSignature(userOp, userOpHash);
+            // _validateNonce()
+            _payPrefund(missingAccountFunds);
+        }
+
+        // EIP-191 version of the signed hash
+        function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOphash)
+            internal
+            view
+            returns (uint256 validationData)
+        {
+            bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOphash);
+            address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+            if(signer != owner()) {
+                return SIG_VALIDATION_FAILED;
+            }
+            return SIG_VALIDATION_SUCCESS;
+        }
+
+        function _payPrefund(uint256 missingAccountFunds) internal {
+            if(missingAccountFunds != 0) {
+                (bool success, ) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+                (success);
+            }
+        }
+
+        /*//////////////////////////////////////////////////////////////
+                            GETTERS
+        //////////////////////////////////////////////////////////////*/
+        function getEntryPoint() external view returns (address) {
+            return address(i_entryPoint);
+        }
+}
