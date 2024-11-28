74a85a8cecd96507e8c3defad9e27550bd715fdf
Daniel <daniel.passage606@passmail.com>
2024-11-27 22:22:24 -0400

    Added test for initialized poll

--- 60-solana-developer-bootcamp-2024/project-2-voting/anchor/tests/voting.spec.ts
+++ 60-solana-developer-bootcamp-2024/project-2-voting/anchor/tests/voting.spec.ts
@@ -34,5 +34,9 @@ describe('Voting', () => {
         const poll = await votingProgram.account.poll.fetch(pollAddress);
 
         console.log(poll);
+
+        expect(poll.pollId.toNumber()).toEqual(1);
+        expect(poll.description).toEqual("What is your favorite type of peanut butter?");
+        expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
     });
 })
