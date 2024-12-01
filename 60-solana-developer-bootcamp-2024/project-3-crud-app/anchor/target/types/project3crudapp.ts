a52e23931e8fdb7091c5bf545dce0ee99d523821
Daniel <daniel.passage606@passmail.com>
2024-11-30 23:44:34 -0400

    Runed build

--- 60-solana-developer-bootcamp-2024/project-3-crud-app/anchor/target/types/project3crudapp.ts
+++ 60-solana-developer-bootcamp-2024/project-3-crud-app/anchor/target/types/project3crudapp.ts
@@ -14,90 +14,36 @@ export type Project3crudapp = {
   },
   "instructions": [
     {
-      "name": "close",
+      "name": "createJournalEntry",
       "discriminator": [
-        98,
-        165,
-        201,
-        177,
-        108,
+        48,
         65,
-        206,
-        96
+        201,
+        186,
+        25,
+        41,
+        127,
+        0
       ],
       "accounts": [
         {
-          "name": "payer",
+          "name": "journalEntry",
           "writable": true,
-          "signer": true
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "title"
+              },
+              {
+                "kind": "account",
+                "path": "owner"
+              }
+            ]
+          }
         },
         {
-          "name": "project3crudapp",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "decrement",
-      "discriminator": [
-        106,
-        227,
-        168,
-        59,
-        248,
-        27,
-        150,
-        101
-      ],
-      "accounts": [
-        {
-          "name": "project3crudapp",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "increment",
-      "discriminator": [
-        11,
-        18,
-        104,
-        9,
-        104,
-        174,
-        59,
-        33
-      ],
-      "accounts": [
-        {
-          "name": "project3crudapp",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "initialize",
-      "discriminator": [
-        175,
-        175,
-        109,
-        31,
-        13,
-        152,
-        155,
-        237
-      ],
-      "accounts": [
-        {
-          "name": "payer",
-          "writable": true,
-          "signer": true
-        },
-        {
-          "name": "project3crudapp",
+          "name": "owner",
           "writable": true,
           "signer": true
         },
@@ -106,58 +52,146 @@ export type Project3crudapp = {
           "address": "11111111111111111111111111111111"
         }
       ],
-      "args": []
+      "args": [
+        {
+          "name": "title",
+          "type": "string"
+        },
+        {
+          "name": "message",
+          "type": "string"
+        }
+      ]
     },
     {
-      "name": "set",
+      "name": "deleteJournalEntry",
       "discriminator": [
-        198,
-        51,
-        53,
-        241,
-        116,
-        29,
-        126,
-        194
+        156,
+        50,
+        93,
+        5,
+        157,
+        97,
+        188,
+        114
       ],
       "accounts": [
         {
-          "name": "project3crudapp",
-          "writable": true
+          "name": "journalEntry",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "title"
+              },
+              {
+                "kind": "account",
+                "path": "owner"
+              }
+            ]
+          }
+        },
+        {
+          "name": "owner",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgam",
+          "address": "11111111111111111111111111111111"
         }
       ],
       "args": [
         {
-          "name": "value",
-          "type": "u8"
+          "name": "title",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "updateJournalEntry",
+      "discriminator": [
+        113,
+        164,
+        49,
+        62,
+        43,
+        83,
+        194,
+        172
+      ],
+      "accounts": [
+        {
+          "name": "journalEntry",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "title"
+              },
+              {
+                "kind": "account",
+                "path": "owner"
+              }
+            ]
+          }
+        },
+        {
+          "name": "owner",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "title",
+          "type": "string"
+        },
+        {
+          "name": "message",
+          "type": "string"
         }
       ]
     }
   ],
   "accounts": [
     {
-      "name": "project3crudapp",
+      "name": "journalEntryState",
       "discriminator": [
-        255,
-        176,
-        4,
-        245,
-        188,
-        253,
+        113,
+        86,
+        110,
         124,
-        25
+        140,
+        14,
+        58,
+        66
       ]
     }
   ],
   "types": [
     {
-      "name": "project3crudapp",
+      "name": "journalEntryState",
       "type": {
         "kind": "struct",
         "fields": [
           {
-            "name": "count",
-            "type": "u8"
+            "name": "owner",
+            "type": "pubkey"
+          },
+          {
+            "name": "title",
+            "type": "string"
+          },
+          {
+            "name": "message",
+            "type": "string"
           }
         ]
       }
