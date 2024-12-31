2e691d5d368d5e52cd41a2bd55fb1c64cbf00d1e
Daniel <daniel.passage606@passmail.com>
2024-12-31 16:32:58 -0400

    Add ABI encode example

--- 59-security-and-auditing-full-course/1-basic-solc-project/src/Encoding.sol
+++ 59-security-and-auditing-full-course/1-basic-solc-project/src/Encoding.sol
@@ -22,8 +22,40 @@ contract Encoding {
     return someString;
   }
 
-    function encodeStringPacked() public pure returns(bytes memory) {
-        bytes memory someString = abi.encodePacked("Some string");
-        return someString;
-    }
+  function encodeStringBytes() public pure returns(bytes memory) {
+    bytes memory someString = bytes("Some string");
+    return someString;
+  }
+
+  function decodeString() public pure returns(string memory) {
+    string memory someString = abi.decode(encodeString(), (string));
+    return someString;
+  }
+
+  function multiEncode() public pure returns(bytes memory) {
+    bytes memory someString = abi.encode("some string", "it's bigger!");
+    return someString;
+  }
+
+  function multiDecode() public pure returns(string memory, string memory) {
+    (string memory someString, string memory someOtherString) = abi.decode(multiEncode(), (string, string));
+    return (someString, someOtherString);
+  }
+
+  function multiEncodePacked() public pure returns(bytes memory) {
+    bytes memory someString = abi.encodePacked("some string", " it's bigger!");
+    return someString;
+  }
+
+  // This doesn't work!
+  function multiDecodePacked() public pure returns(string memory) {
+    string memory someString = abi.decode(multiEncodePacked(), (string));
+    return someString;
+  }
+
+  function multiStringCastPacked() public pure returns(string memory) {
+    string memory someString = string(multiEncodePacked());
+    return someString;
+  }
+
 }
