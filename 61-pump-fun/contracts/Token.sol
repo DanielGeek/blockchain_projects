8ff3aa1f9aba2dbcd2a878b97f896e640c83e999
Daniel <daniel.passage606@passmail.com>
2025-01-03 05:26:24 -0400

    Add ERC20 token contract

--- 61-pump-fun/contracts/Token.sol
+++ 61-pump-fun/contracts/Token.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Token {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    address payable public owner;
+    address public creator;
+
+    constructor(
+        address _creator,
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) ERC20(_name, _symbol) {
+        owner = payable(msg.sender);
+        creator = _creator;
+
+        _mint(msg.sender, _totalSupply);
+    }
+}
