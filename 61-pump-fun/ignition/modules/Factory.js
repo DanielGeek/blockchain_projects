3834f4ed2d43c2ef735e10b1bbc8b78b98e69249
Daniel <daniel.passage606@passmail.com>
2025-01-04 12:56:10 -0400

    Add script for deploy contract factory

--- 61-pump-fun/ignition/modules/Factory.js
+++ 61-pump-fun/ignition/modules/Factory.js
@@ -4,6 +4,16 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("hardhat");
 
+const FEE = ethers.parseUnits("0.01", 18);
+
 module.exports = buildModule("FactoryModule", (m) => {
+    // Get parameters
+    const fee = m.getParameter("fee", FEE);
+
+    // Define factory
+    const factory = m.contract("Factory", [fee]);
+
+    // Return factory
+    return { factory };
 
 })
