b74b35a81677b146f2dde5076b9024acbe147744
Daniel <daniel.passage606@passmail.com>
2025-01-05 09:00:47 -0400

    Add list token and create

--- 61-pump-fun/app/components/List.js
+++ 61-pump-fun/app/components/List.js
@@ -2,9 +2,31 @@ import { ethers } from "ethers"
 
 function List({ toggleCreate, fee, provider, factory }) {
 
+  async function listHandler(form) {
+    const name = form.get("name");
+    const ticker = form.get("ticker");
+
+    const signer = await provider.getSigner();
+
+    const transaction = await factory.connect(signer).create(name, ticker, { value: fee });
+    await transaction.wait();
+
+    toggleCreate();
+  }
+
   return (
     <div className="list">
-      <p>Create token</p>
+      <h2>list new token</h2>
+
+      <div className="list_description">
+        <p>fee: {ethers.formatUnits(fee, 18)} ETH</p>
+      </div>
+
+      <form action={listHandler}>
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="ticker" placeholder="ticker" />
+        <input type="submit" value="[ list ]" />
+      </form>
 
       <button onClick={toggleCreate} className="btn--fancy">[ cancel ]</button>
     </div>
