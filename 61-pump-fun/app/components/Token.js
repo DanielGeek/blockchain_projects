7bc65b3153af4190cd532152a6dc80dd8baddb8f
Daniel <daniel.passage606@passmail.com>
2025-01-05 09:29:39 -0400

    Add list tokens view

--- 61-pump-fun/app/components/Token.js
+++ 61-pump-fun/app/components/Token.js
@@ -3,7 +3,12 @@ import { ethers } from "ethers"
 function Token({ toggleTrade, token }) {
   return (
     <button onClick={() => toggleTrade(token)} className="token">
-
+      <div className="token__details">
+        <img src={token.image} alt="token image" width={256} height={256} />
+        <p>created by {token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
+        <p>maket Cap: {ethers.formatUnits(token.raised, 18)} eth</p>
+        <p className="name">{token.name}</p>
+      </div>
     </button>
   );
 }
--- 61-pump-fun/app/page.js
+++ 61-pump-fun/app/page.js
@@ -19,6 +19,7 @@ export default function Home() {
   const [account, setAccount] = useState(null);
   const [factory, setFactory] = useState(null);
   const [fee, setFee] = useState(0);
+  const [tokens, setTokens] = useState([]);
   const [showCreate, setShowCreate] = useState(false);
 
   function toggleCreate() {
@@ -36,6 +37,29 @@ export default function Home() {
 
     const fee = await factory.fee();
     setFee(fee);
+
+    const totalTokens = await factory.totalTokens();
+    const tokens = [];
+
+    for (let i = 0; i < totalTokens; i++) {
+      if (i == 6) {
+        break;
+      }
+
+      const tokenSale = await factory.getTokenSale(i);
+
+      const token = {
+        token: tokenSale.token,
+        name: tokenSale.name,
+        creator: tokenSale.creator,
+        sold: tokenSale.sold,
+        raised: tokenSale.raised,
+        isOpen: tokenSale.isOpen,
+        image: images[i]
+      }
+      tokens.push(token);
+    }
+    setTokens(tokens.reverse());
   }
 
   useEffect(() => {
@@ -58,6 +82,27 @@ export default function Home() {
             )}
           </button>
         </div>
+
+        <div className="listings">
+            <h1>new listing</h1>
+
+            <div className="tokens">
+              {!account ? (
+                <p>Please connect wallet</p>
+              ): tokens.length === 0 ? (
+                <p>No tokens listed</p>
+              ): (
+                tokens.map((token, index) => (
+                  <Token
+                    toggleTrade={() => {}}
+                    token={token}
+                    key={index}
+                  />
+                ))
+              )}
+            </div>
+        </div>
+
       </main>
       {showCreate && (
         <List toggleCreate={toggleCreate} fee={fee} provider={provider} factory={factory} />
