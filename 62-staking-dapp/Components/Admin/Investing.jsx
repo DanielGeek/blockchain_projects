458cc2c1e64198eecfbcb536bb701be78b6665b2
Daniel <daniel.passage606@passmail.com>
2025-01-11 14:52:44 -0400

    Add Investing component

--- 62-staking-dapp/Components/Admin/Investing.jsx
+++ 62-staking-dapp/Components/Admin/Investing.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
-const Investing = () => {
-  return <div>Investing</div>;
+// INTERNAL IMPORT
+import Button from "./RegularComp/ButtonCmp";
+import Notification from "./Notification";
+
+const Investing = ({ poolDetails }) => {
+  return (
+    <div className="tab-pane fade" id="tab-2" role="tabpanel">
+      <div className="row">
+        <div className="col-12">
+          <div className="profile">
+            <ul
+              className="nav nav-tabs section__tabs section__tabs--left"
+              id="section__profile-tabs1"
+              role="tablist"
+            >
+              <Button name={"Active"} tab={"f1"} styleClass="active" />
+            </ul>
+            <div className="tab-content" id="tab-f1" role="tabpanel">
+              <div className="row">
+                <div className="col-12">
+                  {poolDetails?.notifications.map((notify, index) => (
+                    <Notification
+                      key={index}
+                      notify={notify}
+                      poolDetails={poolDetails}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Investing;
