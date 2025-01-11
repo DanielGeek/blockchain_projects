a5a4c41a6c7d26eb4333bcedfd79ec21e2042690
Daniel <daniel.passage606@passmail.com>
2025-01-11 19:01:38 -0400

    Add Notification component

--- 62-staking-dapp/Components/Admin/Notification.jsx
+++ 62-staking-dapp/Components/Admin/Notification.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Notification = () => {
-  return <div>Notification</div>;
+const CURRENCY = process.env.NEXT_PUBLIC_CURRENCY;
+import List from "./RegularComp/List";
+
+const Notification = ({ index, notify, poolDetails }) => {
+  const [percentage, setPercentage] = useState();
+
+  useEffect(() => {
+    const calculatePercentage = () => {
+      const amount = notify?.amount ?? 0;
+
+      const percentageNew = (amount / 100) * 100;
+
+      if (percentageNew === 0) {
+        console.log("Token balance is zero, cannot calculate percentage");
+      } else {
+        setPercentage(percentageNew);
+      }
+    };
+
+    const timer = setTimeout(calculatePercentage, 1000);
+
+    return () => clearTimeout(timer);
+  }, [notify]);
+
+  return (
+    <div key={index} className="deposit">
+      <div className="deposit__name">
+        <span
+          style={{
+            color: "#white",
+          }}
+          className={`deposit__icon deposit__icon--${index == 0 ? "orange" : index == 1 ? "green" : "blue"}`}
+        >
+          {CURRENCY}
+        </span>
+        <h3 className="deposit__title">{notify?.typeOf}</h3>
+      </div>
+
+      <ul className="deposit__list">
+        <List name={"Pool ID"} value={`#00-1${notify?.poolID}`} />
+        <List name={"Date"} value={`#00-1${notify?.timestamp}`} />
+        <List name={"Amount"} value={`#00-1${notify?.amount}`} />
+        <List name={"User"} value={`#00-1${notify?.user}`} />
+      </ul>
+      <div
+        className={`progressbar progressbar--${index == 0 ? "orange" : index == 1 ? "green" : "blue"}`}
+      >
+        <h3 className="progressbar__title">Accrued{notify?.amount} %</h3>
+        <div
+          aria-colspan="progressbar"
+          role="progressbar"
+          aria-valuenow={75}
+          aria-valuemin={0}
+          aria-valuemax={100}
+        >
+         <div
+          className="progressbar-bar proggressbar-bar-striped progressbar-bar-animated"
+          style={{
+            width: `${percentage}%`,
+          }}
+         >
+          <span>{percentage?.toString().slice(0, 2)}</span>
+         </div>
+        </div>
+        <div className="progressbar__values">
+          <span className="progressbar__value progressbar__value--left">0%</span>
+          <span className="progressbar__value progressbar__value--right">100%</span>
+        </div>
+      </div>
+
+      <div className="deposit__profile">
+        <span>Token</span>
+        <p>{poolDetails?.rewardToken.symbol}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Notification;
