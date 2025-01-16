87beab2110f861a618b3e82732f06288bf703973
Daniel <daniel.passage606@passmail.com>
2025-01-16 14:44:18 -0400

    Add Withdraw component

--- 62-staking-dapp/Components/Withdraw.jsx
+++ 62-staking-dapp/Components/Withdraw.jsx
@@ -1,7 +1,89 @@
 import React from "react";
+import { FaRegCopy } from "./ReactICON/index";
 
-const Withdraw = () => {
-  return <div>Withdraw</div>;
+const Withdraw = ({ setWithdrawPoolID, poolDetails }) => {
+  const poolArray = poolDetails?.poolInfoArray ?? [];
+
+  return (
+    <div id="crypto" className="section">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3">
+            <div className="section__title">
+              <h2>Staking reward</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {poolArray.map((pool, index) => (
+            <div
+              key={index}
+              className={`col-12 col-md-6 col-lg-4`}
+            >
+              <div className="node">
+                <h3 className={`node__title node__title--${index == 0
+                    ? "orange"
+                    : index == 1
+                    ? "green"
+                    : index == 2
+                    ? "blue"
+                    : "orange"}`}>
+                    <b>{pool.amount}</b> {pool?.rewardToken.symbol}
+                </h3>
+                <span className="node__date">
+                  {pool?.lockDays} days
+                </span>
+                <span className="node__price">
+                  {" "}
+                  <b>Reward:</b> {pool?.userReward} {pool?.rewardToken.symbol}
+                </span>
+
+                <span className="node__line">
+                  <img src="img/dodgers/dots--line-orange.svg" alt="" />
+                </span>
+                <ul className="node__list">
+                  <li>
+                    <b>{pool?.depositToken.symbol} &nbsp; &nbsp;</b>
+                    {pool?.depositToken.address.slice(0,15)}...<FaRegCopy />
+                  </li>
+                  <li>
+                    <b>{pool?.rewardToken.symbol} &nbsp; &nbsp;</b>
+                    {pool?.rewardToken.address.slice(0,15)}...<FaRegCopy />
+                  </li>
+                  <li>
+                    <b>Current APY: &nbsp; &nbsp;</b>
+                    {pool?.apy}%
+                  </li>
+                  <li>
+                    <b>LR&nbsp;</b>
+                    {pool?.lastRewardAt}&nbsp;
+                    {pool?.rewardToken.symbol}
+                  </li>
+                </ul>
+
+                <button
+                  className="node__btn"
+                  data-bs-target="#modal-node"
+                  type="button"
+                  data-bs-toggle="modal"
+                  onClick={setWithdrawPoolID(
+                    index == 0 ? 0 : index == 1 ? 1 : index == 2 ? 2 : ""
+                  )}
+                >
+                  Withdraw / Claim
+                </button>
+              </div>
+            </div>
+          ))
+          .slice(0, 3)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Withdraw;
