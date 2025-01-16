8e8fb340b3eefb477b1c620ac36b6e01dd874691
Daniel <daniel.passage606@passmail.com>
2025-01-16 16:26:28 -0400

    Add InputRatio component

--- 62-staking-dapp/Components/Admin/RegularComp/InputRatio.jsx
+++ 62-staking-dapp/Components/Admin/RegularComp/InputRatio.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const InputRatio = () => {
-  return <div>InputRatio</div>;
+const InputRatio = ({ value, index }) => {
+  return (
+    <li>
+      <input type="redio" id={`type${index}`} />
+      <label htmlFor={`type${index}`}>{value}</label>
+    </li>
+  );
 };
 
 export default InputRatio;
